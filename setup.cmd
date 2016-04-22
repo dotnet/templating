dd97232170751c839b36d04e47a7d4203d6a9c27
Mike Lorbetske <mike.lorbetske@n3-p.com>
2016-04-21 21:18:17 -0700

    Better selection, more VS template vars

--- setup.cmd
+++ setup.cmd
@@ -2,13 +2,13 @@
 cd src
 dotnet restore
 cd dotnet-new3
-dotnet build -r win10-x64
+dotnet build -r win10-x64 -c Release
 cd ..\Mutant.Chicken.Orchestrator.VsTemplates
-dotnet build
-copy bin\Debug\netstandard1.5\Mutant.Chicken.Runner.dll ..\dotnet-new3\bin\Debug\netcoreapp1.0\win10-x64\Mutant.Chicken.Runner.dll
-copy bin\Debug\netstandard1.5\Mutant.Chicken.Orchestrator.VsTemplates.dll ..\dotnet-new3\bin\Debug\netcoreapp1.0\win10-x64\Mutant.Chicken.Orchestrator.VsTemplates.dll
-cd ..\dotnet-new3\bin\Debug\netcoreapp1.0\win10-x64
-SET PATH=%PATH%;%CD%
+dotnet build -c Release
+copy bin\Release\netstandard1.5\Mutant.Chicken.Runner.dll ..\dotnet-new3\bin\Release\netcoreapp1.0\win10-x64\Mutant.Chicken.Runner.dll
+copy bin\Release\netstandard1.5\Mutant.Chicken.Orchestrator.VsTemplates.dll ..\dotnet-new3\bin\Release\netcoreapp1.0\win10-x64\Mutant.Chicken.Orchestrator.VsTemplates.dll
+cd ..\dotnet-new3\bin\Release\netcoreapp1.0\win10-x64
+SET PATH=%CD%;%PATH%
 dotnet new3 reset
 dotnet new3 component add "dotnet-new3, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"
 dotnet new3 component add "Mutant.Chicken.Orchestrator.VsTemplates, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"
