3ed77a9a2cc8622e38d474467278ba4c76a6390b
Mike Lorbetske <mike.lorbetske@n3-p.com>
2016-05-20 16:19:33 -0700

    Really easy setup, builtins, help fix

--- setup.cmd
+++ setup.cmd
@@ -18,38 +18,38 @@ echo Building dotnet new3...
 cd dotnet-new3
 dotnet build -r win10-x64 -c %DN3B% 1>nul
 
+echo Creating local feed...
+mkdir %~dp0\src\dotnet-new3\bin\%DN3B%\netcoreapp1.0\win10-x64\BuiltIns
+
 echo Building core...
 cd ..\Microsoft.TemplateEngine.Core
 dotnet build -c %DN3B% 1>nul
-
 echo Packing core...
-dotnet pack -c %DN3B% -o %~dp0\feed 1>nul
+dotnet pack -c %DN3B% -o %~dp0\src\dotnet-new3\bin\%DN3B%\netcoreapp1.0\win10-x64\BuiltIns\ 1>nul
 
 echo Building abstractions...
 cd ..\Microsoft.TemplateEngine.Abstractions
 dotnet build -c %DN3B% 1>nul
-
 echo Packing abstractions...
-dotnet pack -c %DN3B% -o %~dp0\feed 1>nul
+dotnet pack -c %DN3B% -o %~dp0\src\dotnet-new3\bin\%DN3B%\netcoreapp1.0\win10-x64\BuiltIns\ 1>nul
 
 echo Building runner...
 cd ..\Microsoft.TemplateEngine.Runner
 dotnet build -c %DN3B% 1>nul
-
 echo Packing runner...
-dotnet pack -c %DN3B% -o %~dp0\feed 1>nul
+dotnet pack -c %DN3B% -o %~dp0\src\dotnet-new3\bin\%DN3B%\netcoreapp1.0\win10-x64\BuiltIns\ 1>nul
 
 echo Building VS Template support...
 cd ..\Microsoft.TemplateEngine.Orchestrator.VsTemplates
 dotnet build -c %DN3B% 1>nul
 echo Packing VS Template Support...
-dotnet pack -c %DN3B% -o %~dp0\feed 1>nul
+dotnet pack -c %DN3B% -o %~dp0\src\dotnet-new3\bin\%DN3B%\netcoreapp1.0\win10-x64\BuiltIns\ 1>nul
 
 echo Building Runnable Project support...
 cd ..\Microsoft.TemplateEngine.Orchestrator.RunnableProjects
 dotnet build -c %DN3B% 1>nul
 echo Packing Runnable Project Support...
-dotnet pack -c %DN3B% -o %~dp0\feed 1>nul
+dotnet pack -c %DN3B% -o %~dp0\src\dotnet-new3\bin\%DN3B%\netcoreapp1.0\win10-x64\BuiltIns\ 1>nul
 
 echo Artifacts built and placed.
 
@@ -59,16 +59,11 @@ echo Updating path...
 IF "%OLDPATH%" == "" (SET "OLDPATH=%PATH%")
 SET "PATH=%CD%;%OLDPATH%"
 
-IF "%DN3RESET%" == "" (
-echo Resetting to defaults...
-dotnet new3 --reset
+echo Copying templates...
+COPY %~dp0\template_feed\*.nupkg %~dp0\src\dotnet-new3\bin\%DN3B%\netcoreapp1.0\win10-x64\BuiltIns /Y 1>nul
 
-echo Updating sources...
-mkdir %userprofile%\.netnew\Templates
-dotnet new3 -i %userprofile%\.netnew\Templates
-COPY "%~dp0\userdir.nuget.config" "%userprofile%\.netnew\nuget.config" /Y
-SET DN3RESET=Done
-)
+echo Copying configuration for builtins...
+COPY %~dp0\src\dotnet-new3\defaultinstall.*.list %~dp0\src\dotnet-new3\bin\%DN3B%\netcoreapp1.0\win10-x64 /Y 1>nul
 
 echo Done.
 POPD
