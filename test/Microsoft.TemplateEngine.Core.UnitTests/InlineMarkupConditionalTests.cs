5704e2ef4ad632e697dfd9561f6bbf95740e5a90
Mike Lorbetske <mlorbe@microsoft.com>
2016-09-11 23:47:41 -0700

    Inline XML conditionals with C++ evaluator

---
+++ test/Microsoft.TemplateEngine.Core.UnitTests/InlineMarkupConditionalTests.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.TemplateEngine.Core.Contracts;
+using Microsoft.TemplateEngine.Core.Expressions.Cpp;
+using Microsoft.TemplateEngine.Core.Operations;
+using Microsoft.TemplateEngine.Core.Util;
+using Xunit;
+
+namespace Microsoft.TemplateEngine.Core.UnitTests
+{
+    public class InlineMarkupConditionalTests : TestBase
+    {
+        private static IProcessor SetupMSBuildProcessor(IVariableCollection vc)
+        {
+            EngineConfig cfg = new EngineConfig(vc, "$({0})");
+            return Processor.Create(cfg, new InlineMarkupConditional(
+                new MarkupTokens("<", "</", ">", "/>", "Condition=\"", "\""),
+                true,
+                true,
+                CppStyleEvaluatorDefinition.CppStyleEvaluator,
+                null
+            ));
+        }
+
+        [Fact]
+        public void VerifyInlineMarkupTrue()
+        {
+            string originalValue = @"<root>
+    <element Condition=""$(FIRST_IF)"" />
+    <element Condition=""$(SECOND_IF)"">
+        <child>
+            <grandchild />
+        </child>
+    </element>
+</root>";
+
+            string expectedValue = @"<root>
+    <element />
+    <element>
+        <child>
+            <grandchild />
+        </child>
+    </element>
+</root>";
+            VariableCollection vc = new VariableCollection
+            {
+                ["FIRST_IF"] = true,
+                ["SECOND_IF"] = true
+            };
+            IProcessor processor = SetupMSBuildProcessor(vc);
+            RunAndVerify(originalValue, expectedValue, processor, 9999);
+        }
+
+        [Fact]
+        public void VerifyInlineMarkupSelfClosedFalse()
+        {
+            string originalValue = @"<root>
+    <element Condition=""$(FIRST_IF)"" />
+    <element Condition=""$(SECOND_IF)"">
+        <child>
+            <grandchild />
+        </child>
+    </element>
+</root>";
+
+            string expectedValue = @"<root>
+    <element>
+        <child>
+            <grandchild />
+        </child>
+    </element>
+</root>";
+            VariableCollection vc = new VariableCollection
+            {
+                ["FIRST_IF"] = false,
+                ["SECOND_IF"] = true
+            };
+            IProcessor processor = SetupMSBuildProcessor(vc);
+            RunAndVerify(originalValue, expectedValue, processor, 9999);
+        }
+
+        [Fact]
+        public void VerifyInlineMarkupElementWithChildrenFalse()
+        {
+            string originalValue = @"<root>
+    <element Condition=""$(FIRST_IF)"" />
+    <element Condition=""$(SECOND_IF)"">
+        <child>
+            <grandchild />
+        </child>
+    </element>
+</root>";
+
+            string expectedValue = @"<root>
+    <element />
+</root>";
+            VariableCollection vc = new VariableCollection
+            {
+                ["FIRST_IF"] = true,
+                ["SECOND_IF"] = false
+            };
+            IProcessor processor = SetupMSBuildProcessor(vc);
+            RunAndVerify(originalValue, expectedValue, processor, 9999);
+        }
+
+        [Fact]
+        public void VerifyInlineMarkupFalse()
+        {
+            string originalValue = @"<root>
+    <element Condition=""$(FIRST_IF)"" />
+    <element Condition=""$(SECOND_IF)"">
+        <child>
+            <grandchild />
+        </child>
+    </element>
+</root>";
+
+            string expectedValue = @"<root>
+</root>";
+            VariableCollection vc = new VariableCollection
+            {
+                ["FIRST_IF"] = false,
+                ["SECOND_IF"] = false
+            };
+            IProcessor processor = SetupMSBuildProcessor(vc);
+            RunAndVerify(originalValue, expectedValue, processor, 9999);
+        }
+    }
+}
