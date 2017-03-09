7fbdeaefea3ca5c0a8d12daa3e81854cbbf79751
seancpeters <sepeters@microsoft.com>
2017-03-09 15:24:18 -0800

    Update to Source include, copy only, exclude - to correctly handle modifiers

---
+++ src/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/ListGlobbingPatternMatcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.TemplateEngine.Core.Contracts;
+
+namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects
+{
+    public class ListGlobbingPatternMatcher : IPathMatcher
+    {
+        public ListGlobbingPatternMatcher(IList<string> patternList)
+        {
+            List<IPathMatcher> pathMatchers = new List<IPathMatcher>();
+
+            foreach (string pattern in patternList)
+            {
+                pathMatchers.Add(new GlobbingPatternMatcher(pattern));
+            }
+
+            _pathMatchers = pathMatchers;
+        }
+
+        private readonly IReadOnlyList<IPathMatcher> _pathMatchers;
+
+        public string Pattern
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsMatch(string path)
+        {
+            foreach (IPathMatcher matcher in _pathMatchers)
+            {
+                if (matcher.IsMatch(path))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
