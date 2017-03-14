9bd0bd80827c1433aeddf9c7d0f33413e378c91b
Mike Lorbetske <mike.lorbetske@n3-p.com>
2017-03-14 00:20:27 -0700

    Pull the CLI to build with from master

--- build.ps1
+++ build.ps1
@@ -44,7 +44,7 @@ if (Test-Path "$RepoRoot\artifacts")
 
 mkdir "$RepoRoot\artifacts" | Out-Null
 
-$DOTNET_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1"
+$DOTNET_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1"
 Invoke-WebRequest $DOTNET_INSTALL_SCRIPT_URL -OutFile "$RepoRoot\artifacts\dotnet-install.ps1"
 
 & "$RepoRoot\artifacts\dotnet-install.ps1" -Verbose
