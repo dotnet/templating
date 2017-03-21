e6ea400be95d2aa6c329ccafda73f6e8c2189305
Mike Lorbetske <mike.lorbetske@n3-p.com>
2017-03-21 10:13:47 -0700

    Add replacement with lookarounds to the kitchen sink tests

--- test/Microsoft.TemplateEngine.Cli.UnitTests/KitchenSinkTests.cs
+++ test/Microsoft.TemplateEngine.Cli.UnitTests/KitchenSinkTests.cs
@@ -5,7 +5,7 @@ namespace Microsoft.TemplateEngine.Cli.UnitTests
     public class KitchenSinkTests : EndToEndTestBase
     {
         [Theory(DisplayName = nameof(ConfigurationTest))]
-        [InlineData("TestAssets.ConfigurationKitchenSink --replaceThings Stuff", "ConfigurationKitchenSink.json")]
+        [InlineData("TestAssets.ConfigurationKitchenSink --replaceThings Stuff --replaceThere You", "ConfigurationKitchenSink.json")]
         public void ConfigurationTest(string args, params string[] scripts)
         {
             Run(args, scripts);
