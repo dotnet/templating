ee68f4533bc8bdde0ccda3baabd9f0977dd21879
Mike Lorbetske <mike.lorbetske@n3-p.com>
2017-04-15 10:01:19 -0700

    Make resets only clean up hives from tools built in this repo, make path change for current cmd session instead of requiring new cmd session

--- build.ps1
+++ build.ps1
@@ -86,6 +86,4 @@ $NoTimestampPackageVersion=$env:PACKAGE_VERSION + "-" + $env:BUILD_QUALITY
 $TimestampPackageVersion=$NoTimestampPackageVersion + "-" + [System.DateTime]::Now.ToString("yyyyMMdd") + "-" + $env:BUILD_NUMBER
 
 & dotnet msbuild $RepoRoot\build.proj /p:IsFullFrameworkBuildSupported=$PerformFullFrameworkBuild /p:New3RuntimeIdentifier=$Runtime /p:Configuration=$Configuration
-$NewPath = $DevDir + ";" + (($env:PATH.Split(';') | where {-not $_.StartsWith($RepoRoot)}) -join ";")
-[Environment]::SetEnvironmentVariable("Path", "$NewPath", [System.EnvironmentVariableTarget]::User)
-$env:Path = $NewPath
+& $RepoRoot\SetPath.ps1 -DevDir "$DevDir"
