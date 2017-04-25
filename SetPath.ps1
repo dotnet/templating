24858e1ba424098e72fd45575e340486716a3105
Mike Lorbetske <mike.lorbetske@n3-p.com>
2017-04-24 22:07:11 -0700

    Fix mac build by explicitly specifying package order
    Fix setup path issue on windows
    
    Fix #665
    
    Update ASP.NET package versions to latest
    
    Add baselines to classlib templates
    
    Disable browserlink in F# mvc

--- SetPath.ps1
+++ SetPath.ps1
@@ -2,11 +2,13 @@ param(
     [switch]$ComputeOnly=$false,
     [string]$DevDir)
 
-$NewPath = $DevDir + ";" + (($env:PATH.Split(';') | where {-not $_.StartsWith($PSScriptRoot)}) -join ";")
+$NewPath = $DevDir + ";" + (($env:PATH.Split(';') | select -unique | where {-not $_.StartsWith($PSScriptRoot)}) -join ";")
 if (!$ComputeOnly) {
   [Environment]::SetEnvironmentVariable("Path", "$NewPath", [System.EnvironmentVariableTarget]::User)
   $env:Path = $NewPath
 }
 else {
-  write-host $NewPath
+  $x = [string]::Concat('SET "PATH=', $NewPath, '"')
+  $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+  [System.IO.File]::WriteAllLines("$PSScriptRoot\artifacts\NewPath.bat", $x, $Utf8NoBomEncoding)
 }
