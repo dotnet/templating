5421a4daecb73b00c06a8e0addfcad5d7f9845aa
mlorbetske <mike.lorbetske@n3-p.com>
2017-05-19 01:42:32 -0700

    Get macos dev experience back to good

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ if [ "$OS" == "ubuntu.16.04" ]; then
     SPECIAL_OS="YES"
 fi
 
-if [ "$SPECIAL_OS" == "YES" ]; then 
+if [ "$SPECIAL_OS" == "YES" ]; then
     RID="$OS-$ARCH"
 fi
 
@@ -18,19 +18,12 @@ echo "Using RID: $RID"
 echo "Using OS: $OS"
 
 CWD="$( pwd )"
-if [ ! "$OS" == "osx.10.11" ]; then
-    DN3BASEDIR0="$( realpath ${BASH_SOURCE[0]} )"
-else
-    DN3BASEDIR0=$BASH_SOURCE[0]
-fi
-DN3BASEDIR1="$( dirname $DN3BASEDIR0 )"
-DN3BASEDIR="$( cd $DN3BASEDIR1 && pwd )"
 
 if [ -z $DN3B ]; then
 DN3B="Debug"; export DN3B
 fi
 
-cd "$DN3BASEDIR"
+cd "$REPOROOT"
 
 echo Using build configuration "$DN3B"
 /bin/bash $REPOROOT/harderreset.sh
@@ -42,26 +35,26 @@ mkdir dev/BuiltIns
 $REPOROOT/build.sh -c $DN3B -r $RID
 
 echo Importing built in templates...
-cp -r "$DN3BASEDIR/template_feed/"* "$DN3BASEDIR/dev/BuiltIns/"
+cp -r "$REPOROOT/template_feed/"* "$REPOROOT/dev/BuiltIns/"
 echo Done!
 
 if [ ! -L /usr/local/bin/setup.sh ]; then
-echo "Creating symbolic link /usr/local/bin/setup.sh -> $DN3BASEDIR/setup.sh"
-sudo ln -s "$DN3BASEDIR/setup.sh" /usr/local/bin/setup.sh
+echo "Creating symbolic link /usr/local/bin/setup.sh -> $REPOROOT/setup.sh"
+ln -s "$REPOROOT/setup.sh" /usr/local/bin/setup.sh
 fi
 
 echo You can now use setup.sh from anywhere to rebuild dotnet new3
 
 if [ ! -L /usr/local/bin/harderreset.sh ]; then
-echo "Creating symbolic link /usr/local/bin/harderreset.sh -> $DN3BASEDIR/harderreset.sh"
-sudo ln -s "$DN3BASEDIR/harderreset.sh" /usr/local/bin/harderreset.sh
+echo "Creating symbolic link /usr/local/bin/harderreset.sh -> $REPOROOT/harderreset.sh"
+ln -s "$REPOROOT/harderreset.sh" /usr/local/bin/harderreset.sh
 fi
 
 echo You can now use harderreset.sh from anywhere to delete dotnet new3 artifacts
 
 if [ ! -L /usr/local/bin/dotnet-new3 ]; then
-echo "Creating symbolic link /usr/local/bin/dotnet-new3 -> $DN3BASEDIR/dev/dotnet-new3"
-sudo ln -s "$DN3BASEDIR/dev/dotnet-new3" /usr/local/bin/dotnet-new3 
+echo "Creating symbolic link /usr/local/bin/dotnet-new3 -> $REPOROOT/dev/dotnet-new3"
+ln -s "$REPOROOT/dev/dotnet-new3" /usr/local/bin/dotnet-new3
 fi
 
 cd $CWD
