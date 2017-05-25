e3668ae756630133157eb8c94c0a41071443159d
mlorbetske <mike.lorbetske@n3-p.com>
2017-05-24 21:49:33 -0700

    Elevate and set privileges on sources to fix ubuntu

--- setup.sh
+++ setup.sh
@@ -31,22 +31,24 @@ echo Done!
 
 if [ ! -L /usr/local/bin/setup.sh ]; then
 echo "Creating symbolic link /usr/local/bin/setup.sh -> $REPOROOT/setup.sh"
-ln -s "$REPOROOT/setup.sh" /usr/local/bin/setup.sh
+sudo ln -s "$REPOROOT/setup.sh" /usr/local/bin/setup.sh
+chmod 777 "$REPOROOT/setup.sh"
 fi
 
 echo You can now use setup.sh from anywhere to rebuild dotnet new3
 
 if [ ! -L /usr/local/bin/harderreset.sh ]; then
 echo "Creating symbolic link /usr/local/bin/harderreset.sh -> $REPOROOT/harderreset.sh"
-ln -s "$REPOROOT/harderreset.sh" /usr/local/bin/harderreset.sh
+sudo ln -s "$REPOROOT/harderreset.sh" /usr/local/bin/harderreset.sh
+chmod 777 "$REPOROOT/harderreset.sh"
 fi
 
 echo You can now use harderreset.sh from anywhere to delete dotnet new3 artifacts
 
 if [ ! -L /usr/local/bin/dotnet-new3 ]; then
 echo "Creating symbolic link /usr/local/bin/dotnet-new3 -> $REPOROOT/dev/dotnet-new3.sh"
-ln -s "$REPOROOT/dev/dotnet-new3.sh" /usr/local/bin/dotnet-new3
-chmod 777 /usr/local/bin/dotnet-new3
+sudo ln -s "$REPOROOT/dev/dotnet-new3.sh" /usr/local/bin/dotnet-new3
+chmod 777 "$REPOROOT/dev/dotnet-new3.sh"
 fi
 
 cd $CWD
