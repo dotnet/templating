8bb11776be9908eab95dd21f38096523552059d1
seancpeters <sepeters@microsoft.com>
2017-08-08 16:19:21 -0700

    DerivedSymbol implementation - symbol whose value is a value-form transofrm of another symbol value

--- test/Microsoft.TemplateEngine.Cli.UnitTests/FileRenameTests.cs
+++ test/Microsoft.TemplateEngine.Cli.UnitTests/FileRenameTests.cs
@@ -9,6 +9,7 @@ namespace Microsoft.TemplateEngine.Cli.UnitTests
         [InlineData("TestAssets.TemplateWithSourceName --name baz", "FileRenamesTest.json")]
         [InlineData("TestAssets.TemplateWithUnspecifiedSourceName --name baz", "NegativeFileRenamesTest.json")]
         [InlineData("TestAssets.TemplateWithPlaceholderFiles", "TemplateWithPlaceholderFilesTest.json")]
+        [InlineData("TestAssets.TemplateWithDerivedSymbolFileRename --name Last.Part.Is.For.Rename", "DerivedSymbolFileRenameTest.json")]
         public void VerifyTemplateContentRenames(string args, params string[] scripts)
         {
             Run(args, scripts);
