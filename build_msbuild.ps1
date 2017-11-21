d9dcbb415799a5c75831ccbc65f900bb252adaf3
Vijay Ramakrishnan <vramak@microsoft.com>
2017-11-21 15:40:56 -0800

    Adding build scripts to use full msbuild

---
+++ build_msbuild.ps1
@@ -0,0 +1,99 @@
+#
+# Copyright (c) .NET Foundation and contributors. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#
+
+param(
+    [string]$Configuration="Debug",
+    [bool]$PerformFullFrameworkBuild=$true,
+    [bool]$CIBuild=$false,
+    [bool]$SkipTests=$false,
+    [bool]$TemplatesBuild=-not $CIBuild,
+    [bool]$EngineBuild=-not $CIBuild,
+    [switch]$Help)
+
+if($Help)
+{
+    Write-Host "Usage: .\build.ps1 [-Configuration <CONFIGURATION>] [-Help]"
+    Write-Host ""
+    Write-Host "Options:"
+    Write-Host "  -Configuration <CONFIGURATION>                Build the specified Configuration (Debug or Release, default: Debug)"
+    Write-Host "  -PerformFullFrameworkBuild <$true|$false>     Whether or not to build for NET45 as well as .NET Core"
+    Write-Host "  -Help                                         Display this help message"
+    exit 0
+}
+
+if (!$PerformFullFrameworkBuild)
+{
+    Write-Host "Skipping NET45 build..."
+}
+else
+{
+    Write-Host "NET45 build is enabled, if invoking from setup, set the environment variable DN3FFB to $false to prevent this..."
+}
+
+$RepoRoot = "$PSScriptRoot"
+$ArtifactsDir = "$RepoRoot\artifacts"
+$DevDir = "$RepoRoot\dev"
+$env:CONFIGURATION = $Configuration;
+
+rm "$DevDir" -Force -Recurse
+if(Test-Path "$DevDir") { throw "Failed to remove 'dev'" }
+
+# Use a repo-local install directory (but not the artifacts directory because that gets cleaned a lot
+if (!$env:DOTNET_INSTALL_DIR)
+{
+    $env:DOTNET_INSTALL_DIR="$RepoRoot\.dotnet\"
+}
+
+if (!(Test-Path $env:DOTNET_INSTALL_DIR))
+{
+    mkdir $env:DOTNET_INSTALL_DIR | Out-Null
+}
+
+if (Test-Path "$RepoRoot\artifacts")
+{
+    rm "$RepoRoot\artifacts" -Force -Recurse
+}
+
+mkdir "$RepoRoot\artifacts" | Out-Null
+
+$DOTNET_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1"
+Invoke-WebRequest $DOTNET_INSTALL_SCRIPT_URL -OutFile "$RepoRoot\artifacts\dotnet-install.ps1"
+
+& "$RepoRoot\artifacts\dotnet-install.ps1" -Verbose -Version 1.0.4
+if($LASTEXITCODE -ne 0) { throw "Failed to install dotnet cli" }
+
+# Put the stage0 on the path
+$env:PATH = "$env:DOTNET_INSTALL_DIR;$env:PATH"
+
+if (!$env:MSBuildExePath)
+{
+    $env:MSBuildExePath= "${Env:ProgramFiles(x86)}" + "Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+}
+
+$env:PATH = "$env:MSBuildExePath;$env:PATH"
+
+if (-not $env:BUILD_NUMBER)
+{
+  $env:BUILD_NUMBER = 0
+}
+
+if (-not $env:PACKAGE_VERSION)
+{
+  $env:PACKAGE_VERSION = "1.0.0"
+}
+
+$NoTimestampPackageVersion=$env:PACKAGE_VERSION
+
+if (-not $env:BUILD_QUALITY)
+{
+  $env:BUILD_QUALITY = "beta1"
+}
+
+$NoTimestampPackageVersion=$env:PACKAGE_VERSION + "-" + $env:BUILD_QUALITY
+
+$TimestampPackageVersion=$NoTimestampPackageVersion + "-" + [System.DateTime]::Now.ToString("yyyyMMdd") + "-" + $env:BUILD_NUMBER
+
+& msbuild $RepoRoot\build.proj /p:IsFullFrameworkBuildSupported=$PerformFullFrameworkBuild /p:Configuration=$Configuration /p:CIBuild=$CIBuild /p:SkipTests=$SkipTests /p:TemplatesBuild=$TemplatesBuild /p:EngineBuild=$EngineBuild
+& $RepoRoot\SetPath.ps1 -DevDir "$DevDir"
---
+++ setup_msbuild.cmd
@@ -0,0 +1,49 @@
+@echo off
+
+doskey setup="%~dp0\setup.cmd"
+doskey build="%~dp0\dn3build.cmd"
+doskey debug="%~dp0\dn3buildmode-debug.cmd"
+doskey release="%~dp0\dn3buildmode-release.cmd"
+doskey hardreset="%~dp0\hardreset.cmd"
+doskey harderreset="%~dp0\harderreset.cmd"
+doskey bundle="%~dp0\bundle.cmd"
+doskey bin="%~dp0\bin.cmd"
+
+SET DN3BASEDIR=%~dp0
+
+PUSHD %~dp0\src
+IF "%DN3B%" == "" (SET DN3B=Release)
+echo Using build configuration "%DN3B%"...
+
+IF "%DN3FFB%" == "" (SET DN3FFB=$true)
+
+CALL "%~dp0\harderreset.cmd"
+
+mkdir %~dp0\dev 1>nul
+
+echo "Calling build.ps1"
+powershell -NoProfile -NoLogo -Command "& \"%~dp0build_msbuild.ps1\" -Configuration %DN3B% -PerformFullFrameworkBuild %DN3FFB% %*; exit $LastExitCode;"
+
+echo Artifacts built and placed.
+
+echo Copying configuration for builtins...
+COPY %~dp0\src\dotnet-new3\defaultinstall.*.list %~dp0\dev /Y 1>nul
+
+echo Deleting NuGet caches...
+for /f %%f in ('dir /AD /B "%USERPROFILE%\.nuget\packages\Microsoft.TemplateEngine.*"') do RMDIR "%USERPROFILE%\.nuget\packages\%%f" /S /Q
+for /f %%f in ('dir /AD /B "%USERPROFILE%\.nuget\packages\Microsoft.DotNet.*.Templates.*"') do RMDIR "%USERPROFILE%\.nuget\packages\%%f" /S /Q
+
+echo Done.
+POPD
+
+powershell -NoProfile -NoLogo -Command "& \"%~dp0SetPath.ps1\" -ComputeOnly -DevDir \"%~dp0dev\"; exit $LastExitCode;"
+call "%~dp0\artifacts\NewPath.bat"
+del "%~dp0\artifacts\NewPath.bat"
+
+echo.
+echo You can now use `setup` from anywhere (in this console session) to run setup again.
+echo You can now use `build` from anywhere (in this console session) to build dotnet-new3 in the current configuration.
+echo You can now use `debug` from anywhere (in this console session) to change the active build configuration to DEBUG.
+echo You can now use `release` from anywhere (in this console session) to change the active build configuration to RELEASE.
+echo.
+echo dotnet new3 is ready!
