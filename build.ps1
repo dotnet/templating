cff1c7642819b768d01fd778f9557d0dc0501b5a
Matt Mitchell <mmitche@microsoft.com>
2018-04-17 09:48:39 -0700

    Remove logging

--- build.ps1
+++ build.ps1
@@ -90,13 +90,13 @@ $TimestampPackageVersion=$NoTimestampPackageVersion + "-" + [System.DateTime]::N
 
 if ($env:UseMSBuildExe -and (Test-Path $env:MSBuildExePath))
 {
-  & $env:MsBuildExePath $RepoRoot\build.proj /p:IsFullFrameworkBuildSupported=$PerformFullFrameworkBuild /p:Configuration=$Configuration /p:CIBuild=$CIBuild /p:SkipTests=$SkipTests /p:TemplatesBuild=$TemplatesBuild /p:EngineBuild=$EngineBuild /v:detailed
+  & $env:MsBuildExePath $RepoRoot\build.proj /p:IsFullFrameworkBuildSupported=$PerformFullFrameworkBuild /p:Configuration=$Configuration /p:CIBuild=$CIBuild /p:SkipTests=$SkipTests /p:TemplatesBuild=$TemplatesBuild /p:EngineBuild=$EngineBuild
 }
 else
 {
-  & dotnet msbuild $RepoRoot\build.proj /p:IsFullFrameworkBuildSupported=$PerformFullFrameworkBuild /p:Configuration=$Configuration /p:CIBuild=$CIBuild /p:SkipTests=$SkipTests /p:TemplatesBuild=$TemplatesBuild /p:EngineBuild=$EngineBuild /v:detailed
+  & dotnet msbuild $RepoRoot\build.proj /p:IsFullFrameworkBuildSupported=$PerformFullFrameworkBuild /p:Configuration=$Configuration /p:CIBuild=$CIBuild /p:SkipTests=$SkipTests /p:TemplatesBuild=$TemplatesBuild /p:EngineBuild=$EngineBuild
 }
 
-& dotnet msbuild $RepoRoot\build.proj /t:Test /p:IsFullFrameworkBuildSupported=$PerformFullFrameworkBuild /p:Configuration=$Configuration /p:CIBuild=$CIBuild /p:SkipTests=$SkipTests /p:TemplatesBuild=$TemplatesBuild /p:EngineBuild=$EngineBuild /v:detailed
+& dotnet msbuild $RepoRoot\build.proj /t:Test /p:IsFullFrameworkBuildSupported=$PerformFullFrameworkBuild /p:Configuration=$Configuration /p:CIBuild=$CIBuild /p:SkipTests=$SkipTests /p:TemplatesBuild=$TemplatesBuild /p:EngineBuild=$EngineBuild
   
 & $RepoRoot\SetPath.ps1 -DevDir "$DevDir"
