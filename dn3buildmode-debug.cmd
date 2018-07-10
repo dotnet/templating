cf4eee92738edc5e49bfba39c68808e6ec64230d
Mike Lorbetske <mlorbe@microsoft.com>
2018-07-10 10:34:25 -0700

    Remove some redundant version specifications and missed target framework changes
    Fix build configuration scripts

--- dn3buildmode-debug.cmd
+++ dn3buildmode-debug.cmd
@@ -1,7 +1,3 @@
 @echo off
 SET DN3B=Debug
 echo dotnet-new3 build configuration set to %DN3B%
-PUSHD %~DP0\src\dotnet-new3\bin\%DN3B%\netcoreapp1.0\win10-x64
-IF "%OLDPATH%" == "" (SET "OLDPATH=%PATH%")
-SET "PATH=%CD%;%OLDPATH%"
-POPD
