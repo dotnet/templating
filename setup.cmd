c587ec405156819249ae2e3f09f69004a6fa33ad
Mike Lorbetske <mlorbe@microsoft.com>
2018-07-09 17:19:45 -0700

    Make build the build process much faster

--- setup.cmd
+++ setup.cmd
@@ -6,12 +6,10 @@ doskey debug="%~dp0\dn3buildmode-debug.cmd"
 doskey release="%~dp0\dn3buildmode-release.cmd"
 doskey hardreset="%~dp0\hardreset.cmd"
 doskey harderreset="%~dp0\harderreset.cmd"
-doskey bundle="%~dp0\bundle.cmd"
-doskey bin="%~dp0\bin.cmd"
 
 SET DN3BASEDIR=%~dp0
 
-PUSHD %~dp0\src
+PUSHD %~dp0
 IF "%DN3B%" == "" (SET DN3B=Release)
 echo Using build configuration "%DN3B%"...
 
@@ -21,8 +19,21 @@ CALL "%~dp0\harderreset.cmd"
 
 mkdir %~dp0\dev 1>nul
 
-echo "Calling build.ps1"
-powershell -NoProfile -NoLogo -Command "& \"%~dp0build.ps1\" -Configuration %DN3B% -PerformFullFrameworkBuild %DN3FFB% %*; exit $LastExitCode;"
+echo Building for full framework
+dotnet msbuild %~dp0\build\CoreBuild.proj /t:GetReady;Restore;Build /p:TargetFramework=net46 /p:Configuration=%DN3B% %*
+
+if %ERRORLEVEL% NEQ 0 (
+    echo BUILD FAILED
+    exit /B
+)
+
+echo Building for .NET Core
+dotnet msbuild %~dp0\build\CoreBuild.proj /t:GetReady;Restore;Build;Pack;RunTests /p:TargetFramework=netcoreapp1.1 /p:Configuration=%DN3B% %*
+
+if %ERRORLEVEL% NEQ 0 (
+    echo BUILD FAILED
+    exit /B
+)
 
 echo Artifacts built and placed.
 
