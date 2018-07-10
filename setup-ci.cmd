563c055023246a19ed1f4152c5b4cf4fb7b4bd8a
Mike Lorbetske <mlorbe@microsoft.com>
2018-07-09 17:19:45 -0700

    Make build the build process much faster

---
+++ setup-ci.cmd
@@ -0,0 +1,28 @@
+@echo off
+SET DN3BASEDIR=%~dp0
+
+PUSHD %~dp0\src
+SET DN3B=Release
+echo Using build configuration "%DN3B%"...
+
+IF "%DN3FFB%" == "" (SET DN3FFB=$true)
+
+CALL "%~dp0\harderreset.cmd"
+
+mkdir %~dp0\dev 1>nul
+
+echo Building for full framework
+dotnet msbuild %~dp0\build\CoreBuild.proj /t:GetReady;Restore;Build /p:TargetFramework=net46 /p:Configuration=%DN3B% %*
+
+if %ERRORLEVEL% NEQ 0 (
+    echo "BUILD FAILED"
+    exit /B
+)
+
+echo Building for .NET Core
+dotnet msbuild %~dp0\build\CoreBuild.proj /t:GetReady;Restore;Build;Pack;RunTests /p:TargetFramework=netcoreapp1.1 /p:Configuration=%DN3B% %*
+
+if %ERRORLEVEL% NEQ 0 (
+    echo "BUILD FAILED"
+    exit /B
+)
