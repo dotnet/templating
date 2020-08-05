eabce106cf2f85355fda9cf22b9b606af190b235
Bekir Ozturk <bozturk@microsoft.com>
2020-08-05 18:01:48 +0200

    IInstaller design proposal

--- src/Microsoft.TemplateEngine.Cli/IInstaller.cs
+++ src/Microsoft.TemplateEngine.Cli/IInstaller.cs
@@ -5,10 +5,13 @@ namespace Microsoft.TemplateEngine.Cli
 {
     public interface IInstaller : IInstallerBase
     {
-        void InstallPackages(IEnumerable<string> installationRequests, IList<string> nuGetSources);
+        /// <param name="optionalWorkloadRequests">Requests from <paramref name="installationRequests"/> which are part of an optional workload.</param>
+        void InstallPackages(IEnumerable<string> installationRequests, IList<string> optionalWorkloadRequests, IList<string> nuGetSources);
 
-        void InstallPackages(IEnumerable<string> installationRequests, IList<string> nuGetSources, bool debugAllowDevInstall);
+        /// <param name="optionalWorkloadRequests">Requests from <paramref name="installationRequests"/> which are part of an optional workload.</param>
+        void InstallPackages(IEnumerable<string> installationRequests, IList<string> optionalWorkloadRequests, IList<string> nuGetSources, bool debugAllowDevInstall);
 
-        void InstallPackages(IEnumerable<string> installationRequests, IList<string> nuGetSources, bool debugAllowDevInstall, bool interactive);
+        /// <param name="optionalWorkloadRequests">Requests from <paramref name="installationRequests"/> which are part of an optional workload.</param>
+        void InstallPackages(IEnumerable<string> installationRequests, IList<string> optionalWorkloadRequests, IList<string> nuGetSources, bool debugAllowDevInstall, bool interactive);
     }
 }
