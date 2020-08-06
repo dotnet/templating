e398fd84f566f5c39862f8417f523aae5b972269
Bekir Ozturk <bozturk@microsoft.com>
2020-08-06 08:59:28 +0200

    IInstaller design proposal - new overloads

--- src/Microsoft.TemplateEngine.Cli/IInstaller.cs
+++ src/Microsoft.TemplateEngine.Cli/IInstaller.cs
@@ -5,6 +5,12 @@ namespace Microsoft.TemplateEngine.Cli
 {
     public interface IInstaller : IInstallerBase
     {
+        void InstallPackages(IEnumerable<string> installationRequests, IList<string> nuGetSources);
+
+        void InstallPackages(IEnumerable<string> installationRequests, IList<string> nuGetSources, bool debugAllowDevInstall);
+
+        void InstallPackages(IEnumerable<string> installationRequests, IList<string> nuGetSources, bool debugAllowDevInstall, bool interactive);
+
         /// <param name="optionalWorkloadRequests">Requests from <paramref name="installationRequests"/> which are part of an optional workload.</param>
         void InstallPackages(IEnumerable<string> installationRequests, IList<string> optionalWorkloadRequests, IList<string> nuGetSources);
 
