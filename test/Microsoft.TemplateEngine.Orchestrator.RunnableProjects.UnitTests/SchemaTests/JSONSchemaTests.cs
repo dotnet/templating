115ead8e7cd8edf76139a042e8173d098c9e09aa
Vlada Shubina <vshubina@microsoft.com>
2020-09-25 18:37:53 +0300

    fixed path separator in JSON schema test

--- test/Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests/SchemaTests/JSONSchemaTests.cs
+++ test/Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests/SchemaTests/JSONSchemaTests.cs
@@ -9,13 +9,13 @@ namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests.Schem
     public class JSONSchemaTests
     {
         [Theory(DisplayName = nameof(IsJSONSchemaValid))]
-        [InlineData(@"SchemaTests\BasicTest.json")]
-        [InlineData(@"SchemaTests\GeneratorTest.json")]
-        [InlineData(@"SchemaTests\StarterWebTest.json")]
-        [InlineData(@"SchemaTests\PostActionTest.json")]
+        [InlineData(@"SchemaTests/BasicTest.json")]
+        [InlineData(@"SchemaTests/GeneratorTest.json")]
+        [InlineData(@"SchemaTests/StarterWebTest.json")]
+        [InlineData(@"SchemaTests/PostActionTest.json")]
         public void IsJSONSchemaValid(string testFile)
         {
-            using (TextReader schemaFileStream = File.OpenText(@"SchemaTests\template.json"))
+            using (TextReader schemaFileStream = File.OpenText(@"SchemaTests/template.json"))
             {
                 JSchema schema = JSchema.Load(new JsonTextReader(schemaFileStream));
                 using (TextReader jsonFileStream = File.OpenText(testFile))
