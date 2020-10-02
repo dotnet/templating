b19cf8316319ccddb6d2951fd169a65f117cbb56
Vlada Shubina <vshubina@microsoft.com>
2020-10-02 13:58:42 +0300

    Reverted unchanged files

--- src/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/GlobalRunConfig.cs
+++ src/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/GlobalRunConfig.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.TemplateEngine.Core.Contracts;
 
 namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects
