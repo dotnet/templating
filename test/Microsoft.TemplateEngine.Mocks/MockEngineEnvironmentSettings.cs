3dcd1d81cd4c5cf8c6a1d604f58993da3cf52834
Vlada Shubina <vshubina@microsoft.com>
2020-10-27 13:01:19 +0200

    added tests for HelpFormatter

--- test/Microsoft.TemplateEngine.Mocks/MockEngineEnvironmentSettings.cs
+++ test/Microsoft.TemplateEngine.Mocks/MockEngineEnvironmentSettings.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,10 +9,35 @@ namespace Microsoft.TemplateEngine.Mocks
     {
         public ISettingsLoader SettingsLoader { get { throw new NotImplementedException(); } }
 
-        public ITemplateEngineHost Host { get { throw new NotImplementedException(); } }
+        public ITemplateEngineHost Host { get; set; }
 
-        public IEnvironment Environment { get { throw new NotImplementedException(); } }
+        public IEnvironment Environment { get; set; }
 
         public IPathInfo Paths { get { throw new NotImplementedException(); } }
+    }
+
+    public class MockEnvironment : IEnvironment
+    {
+        public string NewLine => Environment.NewLine;
+
+        public int ConsoleBufferWidth
+        {
+            get; set;
+        }
+
+        public string ExpandEnvironmentVariables(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetEnvironmentVariable(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyDictionary<string, string> GetEnvironmentVariables()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
