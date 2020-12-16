19276d4140ee9869cbe8406d20d539fd8c0afd21
Vlada Shubina <vshubina@microsoft.com>
2020-12-16 15:05:59 +0200

    added new InputParameterFormat property for MatchInfo struct and  OrdinalIgnoreCaseMatchInfoComparer

---
+++ src/Microsoft.TemplateEngine.Edge/Template/OrdinalIgnoreCaseMatchInfoComparer.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.TemplateEngine.Edge.Template
+{
+    public class OrdinalIgnoreCaseMatchInfoComparer : IEqualityComparer<MatchInfo>
+    {
+        public bool Equals(MatchInfo x, MatchInfo y)
+        {
+            if (x.Kind == y.Kind
+                && x.Location == y.Location
+                && string.Equals(x.InputParameterName, y.InputParameterName, StringComparison.OrdinalIgnoreCase)
+                && string.Equals(x.ParameterValue, y.ParameterValue, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int GetHashCode(MatchInfo obj)
+        {
+            return new { a = obj.InputParameterName?.ToLowerInvariant(), b = obj.ParameterValue?.ToLowerInvariant(), obj.Kind, obj.Location }.GetHashCode();
+        }
+    }
+}
