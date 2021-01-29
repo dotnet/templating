75db13c7e2d6c94957cf0659f188fd0aed60b174
Vlada Shubina <vshubina@microsoft.com>
2021-01-29 23:10:06 +0200

    fixed logger

--- src/Microsoft.TemplateEngine.Edge/Installers/NuGet/INuGetLogger.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.TemplateEngine.Abstractions;
-using NuGet.Common;
-
-namespace Microsoft.TemplateEngine.Edge.Installers.NuGet
-{
-    public interface INuGetLogger : IIdentifiedComponent, ILogger
-    {
-    }
-}
