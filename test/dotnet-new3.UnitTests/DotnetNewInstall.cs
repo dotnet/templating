3c7925d627768d7e6649bdf0a9d34bc164c47ff2
Vlada Shubina <vshubina@microsoft.com>
2021-02-02 15:47:32 +0200

    added first E2E install tests

--- test/dotnet-new3.UnitTests/DotnetNewInstall.cs
+++ test/dotnet-new3.UnitTests/DotnetNewInstall.cs
@@ -21,7 +21,7 @@ namespace dotnet_new3.UnitTests
         }
 
         [Fact]
-        public void OnlyPrintNewlyInstalledTemplates()
+        public void CanInstallNupkgFromRemoteSource()
         {
             var home = Helpers.CreateTemporaryFolder("Home");
             // Do actual install...
@@ -33,8 +33,66 @@ namespace dotnet_new3.UnitTests
                 .ExitWith(0)
                 .And
                 .NotHaveStdErr()
+                .And.NotHaveStdOutContaining("Determining projects to restore...")
+                .And.HaveStdOutContaining("web")
+                .And.HaveStdOutContaining("blazorwasm");
+        }
+
+        [Fact]
+        public void PrintOnlyNewlyInstalledTemplates()
+        {
+            var home = Helpers.CreateTemporaryFolder("Home");
+
+            new DotnetNewCommand(_log, "-i", "Microsoft.DotNet.Common.ProjectTemplates.5.0", "--quiet")
+               .WithWorkingDirectory(Helpers.CreateTemporaryFolder())
+               .WithEnvironmentVariable(Helpers.HomeEnvironmentVariableName, home)
+               .Execute()
+               .Should()
+               .ExitWith(0)
+               .And
+               .NotHaveStdErr()
+               .And.HaveStdOutContaining("console")
+               .And.HaveStdOutContaining("Console Application");
+
+            new DotnetNewCommand(_log, "-i", "Microsoft.DotNet.Web.ProjectTemplates.5.0", "--quiet")
+                .WithWorkingDirectory(Helpers.CreateTemporaryFolder())
+                .WithEnvironmentVariable(Helpers.HomeEnvironmentVariableName, home)
+                .Execute()
+                .Should()
+                .ExitWith(0)
                 .And
-                .NotHaveStdOutContaining("Determining projects to restore...");
+                .NotHaveStdErr()
+                .And.HaveStdOutContaining("web")
+                .And.HaveStdOutContaining("blazorwasm")
+                .And.NotHaveStdOutContaining("console");
+        }
+
+        [Fact]
+        public void CannotInstallUnknownRemotePackage()
+        {
+            var home = Helpers.CreateTemporaryFolder("Home");
+
+            new DotnetNewCommand(_log, "-i", "BlaBlaBla", "--quiet")
+               .WithWorkingDirectory(Helpers.CreateTemporaryFolder())
+               .WithEnvironmentVariable(Helpers.HomeEnvironmentVariableName, home)
+               .Execute()
+               .Should()
+               .NotHaveStdOut()
+               .And.HaveStdErrContaining("'BlaBlaBla' could not be installed, the package doesn't exist.");
+        }
+
+        [Fact]
+        public void CannotInstallRemotePackageWithIncorrectVersion()
+        {
+            var home = Helpers.CreateTemporaryFolder("Home");
+
+            new DotnetNewCommand(_log, "-i", "Microsoft.DotNet.Web.ProjectTemplates.5.0::16.0.0", "--quiet")
+               .WithWorkingDirectory(Helpers.CreateTemporaryFolder())
+               .WithEnvironmentVariable(Helpers.HomeEnvironmentVariableName, home)
+               .Execute()
+               .Should()
+               .NotHaveStdOut()
+               .And.HaveStdErrContaining("'Microsoft.DotNet.Web.ProjectTemplates.5.0::16.0.0' could not be installed, the package doesn't exist.");
         }
     }
 }
