de49c3a65e5060fbede508d14af5da1bd7afa9fb
David Karlaš <david.karlas@microsoft.com>
2021-02-05 05:23:06 +0100

    Changed scanning logic to scan only 1 MountPoint and not modify
    Logic of DetermineDirectoriesToScan is now moved to InstallRequestPathResolution which means that we scan only 1 mountpoint via `Scan` method hence:
    - `ScanResult.InstalledMountPointUris` removed
    - `SetupMountPointScanInfoForInstallSourceList` removed
    - Multiple `Scan` overloads removed
    - I never liked `allowDevInstall` hence that also removed
    - Removed logic that prevented having `Components` and `Templates` in same MountPoint
    - Removed logic to copy content in `ScanMountPointForTemplatesAndLangpacks`

--- src/Microsoft.TemplateEngine.Edge/Settings/ScanResult.cs
+++ src/Microsoft.TemplateEngine.Edge/Settings/ScanResult.cs
@@ -11,12 +11,10 @@ namespace Microsoft.TemplateEngine.Edge.Settings
         {
             _localizations = new List<ILocalizationLocator>();
             _templates = new List<ITemplate>();
-            _installedMountPointIds = new HashSet<string>();
         }
 
         private List<ILocalizationLocator> _localizations;
         private List<ITemplate> _templates;
-        private HashSet<string> _installedMountPointIds;
 
         public void AddLocalization(ILocalizationLocator locater)
         {
@@ -28,15 +26,8 @@ namespace Microsoft.TemplateEngine.Edge.Settings
             _templates.Add(template);
         }
 
-        public void AddInstalledMountPointId(string mountPointUri)
-        {
-            _installedMountPointIds.Add(mountPointUri);
-        }
-
         public IReadOnlyList<ILocalizationLocator> Localizations => _localizations;
 
         public IReadOnlyList<ITemplate> Templates => _templates;
-
-        public IReadOnlyList<string> InstalledMountPointUris => _installedMountPointIds.ToList();
     }
 }
