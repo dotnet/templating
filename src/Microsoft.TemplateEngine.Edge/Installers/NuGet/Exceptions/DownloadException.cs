c32f6f70b4e0ea922a7fe3f083d9e2db153fe2c8
Vlada Shubina <vshubina@microsoft.com>
2021-02-09 10:19:47 +0200

    reorg + cleanup

--- src/Microsoft.TemplateEngine.Edge/Installers/NuGet/Exceptions/DownloadException.cs
+++ src/Microsoft.TemplateEngine.Edge/Installers/NuGet/Exceptions/DownloadException.cs
@@ -1,15 +1,14 @@
-using NuGet.Versioning;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using System;
 using System.Collections.Generic;
+using NuGet.Versioning;
 
 namespace Microsoft.TemplateEngine.Edge.Installers.NuGet
 {
     internal class DownloadException : Exception
     {
-        public string PackageIdentifier { get; private set; }
-        public NuGetVersion PackageVersion { get; private set; }
-        public IEnumerable<string> SourcesList { get; private set; }
-
         public DownloadException(string packageIdentifier, NuGetVersion packageVersion, IEnumerable<string> attemptedSources) : base($"Failed to download {packageIdentifier}::{packageVersion} from NuGet feeds {string.Join(";", attemptedSources)}")
         {
             PackageIdentifier = packageIdentifier;
@@ -23,5 +22,9 @@ namespace Microsoft.TemplateEngine.Edge.Installers.NuGet
             PackageVersion = packageVersion;
             SourcesList = attemptedSources;
         }
+
+        public string PackageIdentifier { get; private set; }
+        public NuGetVersion PackageVersion { get; private set; }
+        public IEnumerable<string> SourcesList { get; private set; }
     }
 }
