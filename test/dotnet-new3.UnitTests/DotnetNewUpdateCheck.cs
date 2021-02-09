cc1ee349b0f95910db98540eb6a832dd13e08d2d
Vlada Shubina <vshubina@microsoft.com>
2021-02-09 14:31:42 +0200

    basic e2e test for update check

---
+++ test/dotnet-new3.UnitTests/DotnetNewUpdateCheck.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using FluentAssertions;
+using Microsoft.NET.TestFramework.Assertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace dotnet_new3.UnitTests
+{
+    public class DotnetNewUpdateCheck
+    {
+        private readonly ITestOutputHelper _log;
+
+        public DotnetNewUpdateCheck(ITestOutputHelper log)
+        {
+            _log = log;
+        }
+
+        [Fact]
+        public void CanCheckForUpdate()
+        {
+            var home = Helpers.CreateTemporaryFolder("Home");
+            // Do actual install...
+            new DotnetNewCommand(_log, "-i", "Microsoft.DotNet.Common.ProjectTemplates.5.0::5.0.0", "--quiet")
+                .WithWorkingDirectory(Helpers.CreateTemporaryFolder())
+                .WithEnvironmentVariable(Helpers.HomeEnvironmentVariableName, home)
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And
+                .NotHaveStdErr()
+                .And.NotHaveStdOutContaining("Determining projects to restore...")
+                .And.HaveStdOutContaining("console")
+                .And.HaveStdOutContaining("classlib");
+
+            new DotnetNewCommand(_log, "--update-check")
+                .WithWorkingDirectory(Helpers.CreateTemporaryFolder())
+                .WithEnvironmentVariable(Helpers.HomeEnvironmentVariableName, home)
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And
+                .NotHaveStdErr()
+                .And.HaveStdOutContaining("An update for template pack Microsoft.DotNet.Common.ProjectTemplates.5.0::5.0.0 is available.");
+        }
+    }
+}
