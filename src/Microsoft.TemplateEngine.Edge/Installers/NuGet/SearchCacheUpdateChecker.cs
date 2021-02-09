a1aed1553f7c17ce9b5fe2e948c56724fc76c64f
Vlada Shubina <vshubina@microsoft.com>
2021-02-09 10:20:17 +0200

    get latest version + uninstall implementation

--- src/Microsoft.TemplateEngine.Edge/Installers/NuGet/SearchCacheUpdateChecker.cs
+++ src/Microsoft.TemplateEngine.Edge/Installers/NuGet/SearchCacheUpdateChecker.cs
@@ -1,8 +1,9 @@
-using Microsoft.TemplateEngine.Abstractions;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.TemplateEngine.Abstractions.TemplatesSources;
 
 namespace Microsoft.TemplateEngine.Edge.Installers.NuGet
 {
@@ -13,9 +14,6 @@ namespace Microsoft.TemplateEngine.Edge.Installers.NuGet
             throw new NotImplementedException();
         }
 
-        public Task<string> GetLatestVersionAsync(NuGetManagedTemplatesSource source)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<ManagedTemplatesSourceUpdate> GetLatestVersionAsync(NuGetManagedTemplatesSource source) => throw new NotImplementedException();
     }
 }
