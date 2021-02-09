2ecbb89337a7a827e02b8de4c0696df2fe4e1fee
Vlada Shubina <vshubina@microsoft.com>
2021-02-09 10:19:47 +0200

    reorg + cleanup

--- src/Microsoft.TemplateEngine.Edge/Installers/NuGet/Exceptions/PackageNotFoundException.cs
+++ src/Microsoft.TemplateEngine.Edge/Installers/NuGet/Exceptions/PackageNotFoundException.cs
@@ -1,17 +1,15 @@
-using NuGet.Versioning;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
+using NuGet.Versioning;
 
 namespace Microsoft.TemplateEngine.Edge.Installers.NuGet
 {
     internal class PackageNotFoundException : Exception
     {
-        public string PackageIdentifier { get; private set; }
-        public NuGetVersion PackageVersion { get; private set; }
-        public IEnumerable<string> SourcesList { get; private set; }
-
-        public PackageNotFoundException(string packageIdentifier, IEnumerable<string> attemptedSources) : base ($"{packageIdentifier} was not found in NuGet feeds {string.Join(";", attemptedSources)}")
+        public PackageNotFoundException(string packageIdentifier, IEnumerable<string> attemptedSources) : base($"{packageIdentifier} was not found in NuGet feeds {string.Join(";", attemptedSources)}")
         {
             PackageIdentifier = packageIdentifier;
             SourcesList = attemptedSources;
@@ -23,5 +21,9 @@ namespace Microsoft.TemplateEngine.Edge.Installers.NuGet
             PackageVersion = packageVersion;
             SourcesList = attemptedSources;
         }
+
+        public string PackageIdentifier { get; private set; }
+        public NuGetVersion PackageVersion { get; private set; }
+        public IEnumerable<string> SourcesList { get; private set; }
     }
 }
--- src/Microsoft.TemplateEngine.Edge/Installers/NuGet/NuGetInstallerFactory.cs
+++ src/Microsoft.TemplateEngine.Edge/Installers/NuGet/NuGetInstallerFactory.cs
@@ -1,20 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
 using Microsoft.TemplateEngine.Abstractions;
 using Microsoft.TemplateEngine.Abstractions.Installer;
 using Microsoft.TemplateEngine.Abstractions.TemplatesSources;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Microsoft.TemplateEngine.Edge.Installers.NuGet
 {
-    class NuGetInstallerFactory : IInstallerFactory
+    internal class NuGetInstallerFactory : IInstallerFactory
     {
         public static readonly Guid FactoryId = new Guid("{015DCBAC-B4A5-49EA-94A6-061616EB60E2}");
 
         public Guid Id => FactoryId;
 
         public string Name => "NuGet";
-
 
         public IInstaller CreateInstaller(IManagedTemplatesSourcesProvider provider, IEngineEnvironmentSettings settings, string installPath)
         {
