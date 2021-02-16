ff66c02e174c1a764c66bf7ce49655e64d956170
David Karlaš <david.karlas@microsoft.com>
2021-02-16 11:22:08 +0100

    Moved storing of CurrentVersion from Settings.json to TemplateCache.json itself

--- src/Microsoft.TemplateEngine.Edge/Settings/SettingsStore.cs
+++ src/Microsoft.TemplateEngine.Edge/Settings/SettingsStore.cs
@@ -8,26 +8,16 @@ namespace Microsoft.TemplateEngine.Edge.Settings
 {
     public class SettingsStore
     {
-        // NOTE: when the current version changes, a corresponding change in TemplateInfo.cs is needed, to get the correct template cache version reader to fire.
-        public static readonly string CurrentVersion = "1.0.0.4";
-
         public SettingsStore()
         {
             ComponentGuidToAssemblyQualifiedName = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
             ComponentTypeToGuidList = new Dictionary<string, HashSet<Guid>>();
             ProbingPaths = new HashSet<string>();
-            Version = string.Empty;
         }
 
         public SettingsStore(JObject obj)
             : this()
         {
-            JToken versionToken;
-            if (obj.TryGetValue(nameof(Version), StringComparison.OrdinalIgnoreCase, out versionToken))
-            {
-                Version = versionToken.ToString();
-            }
-
             JToken componentGuidToAssemblyQualifiedNameToken;
             if (obj.TryGetValue("ComponentGuidToAssemblyQualifiedName", StringComparison.OrdinalIgnoreCase, out componentGuidToAssemblyQualifiedNameToken))
             {
@@ -91,14 +81,6 @@ namespace Microsoft.TemplateEngine.Edge.Settings
                 }
             }
         }
-
-        public void SetVersionToCurrent()
-        {
-            Version = CurrentVersion;
-        }
-
-        [JsonProperty]
-        public string Version { get; private set; }
 
         [JsonProperty]
         public Dictionary<string, string> ComponentGuidToAssemblyQualifiedName { get; }
