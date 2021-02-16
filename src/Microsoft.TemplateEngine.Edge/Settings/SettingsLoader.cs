d86445444dddaf4be2c47216b9d8aaba0562095a
David Karlaš <david.karlas@microsoft.com>
2021-02-16 12:24:59 +0100

    Changes in SettingsLoader
    - Removed `UserTemplateCache` and `GetTemplates(Hashset<ITemplateInfo>)` and replaced them with `GetTemplatesAsync`
    - Removed `WriteTemplateCache` that now lives in `TemplateCache`
    - Removed Reload because "Rebuild" takes care of that now
    - Simplified `Save` because Version handling is now part of `TemplateCache` and writing of template cache is now called from "Rebuild"

--- src/Microsoft.TemplateEngine.Edge/Settings/SettingsLoader.cs
+++ src/Microsoft.TemplateEngine.Edge/Settings/SettingsLoader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.TemplateEngine.Abstractions;
 using Microsoft.TemplateEngine.Abstractions.GlobalSettings;
@@ -45,35 +46,10 @@ namespace Microsoft.TemplateEngine.Edge.Settings
 
         public void Save()
         {
-            Save(_userTemplateCache);
-        }
-
-        private void Save(TemplateCache cacheToSave)
-        {
             _paths.WriteAllText(_paths.User.GlobalSettingsFile, JsonConvert.SerializeObject(GlobalSettings, Formatting.Indented));
 
-            // When writing the template caches, we need the existing cache version to read the existing caches for before updating.
-            // so don't update it until after the template caches are written.
-            cacheToSave.WriteTemplateCaches(_userSettings.Version);
-
-            // now it's safe to update the cache version, which is written in the settings file.
-            _userSettings.SetVersionToCurrent();
             JObject serialized = JObject.FromObject(_userSettings);
             _paths.WriteAllText(_paths.User.SettingsFile, serialized.ToString());
-
-            if (_userTemplateCache != cacheToSave)  // object equals
-            {
-                ReloadTemplates();
-            }
-        }
-
-        public TemplateCache UserTemplateCache
-        {
-            get
-            {
-                EnsureLoaded();
-                return _userTemplateCache;
-            }
         }
 
         private void EnsureLoaded()
@@ -178,21 +154,10 @@ namespace Microsoft.TemplateEngine.Edge.Settings
 
             string userTemplateCache;
 
-            if (_paths.Exists(_paths.User.CurrentLocaleTemplateCacheFile))
+            if (_paths.Exists(_paths.User.TemplateCacheFile))
             {
                 using (Timing.Over(_environmentSettings.Host, "Read template cache"))
-                    userTemplateCache = _paths.ReadAllText(_paths.User.CurrentLocaleTemplateCacheFile, "{}");
-            }
-            else if (_paths.Exists(_paths.User.CultureNeutralTemplateCacheFile))
-            {
-                // clone the culture neutral cache
-                // this should not occur if there are any langpacks installed for this culture.
-                // when they got installed, the cache should have been created for that locale.
-                using (Timing.Over(_environmentSettings.Host, "Clone cultural neutral cache"))
-                {
-                    userTemplateCache = _paths.ReadAllText(_paths.User.CultureNeutralTemplateCacheFile, "{}");
-                    _paths.WriteAllText(_paths.User.CurrentLocaleTemplateCacheFile, userTemplateCache);
-                }
+                    userTemplateCache = _paths.ReadAllText(_paths.User.TemplateCacheFile, "{}");
             }
             else
             {
@@ -203,83 +168,71 @@ namespace Microsoft.TemplateEngine.Edge.Settings
             using (Timing.Over(_environmentSettings.Host, "Parse template cache"))
                 parsed = JObject.Parse(userTemplateCache);
             using (Timing.Over(_environmentSettings.Host, "Init template cache"))
-                _userTemplateCache = new TemplateCache(_environmentSettings, parsed, _userSettings.Version);
+                _userTemplateCache = new TemplateCache(_environmentSettings, parsed);
 
             _templatesLoaded = true;
-        }
-
-        public void Reload()
-        {
-            _isLoaded = false;
-            EnsureLoaded();
-            ReloadTemplates();
-        }
-
-        private void UpdateTemplateListFromCache(TemplateCache cache, ISet<ITemplateInfo> templates)
-        {
-            using (Timing.Over(_environmentSettings.Host, "Enumerate infos"))
-                templates.UnionWith(cache.TemplateInfo);
         }
 
         public async Task RebuildCacheFromSettingsIfNotCurrent(bool forceRebuild)
         {
             EnsureLoaded();
-
             var placesThatNeedScanning = new HashSet<string>();
-            var mountsPoints = new Dictionary<string, DateTime?>();
+            var mountsPointsInUseNow = new HashSet<string>();
 
-            foreach (var template in _userTemplateCache.TemplateInfo)
+            if (forceRebuild ||
+                _userTemplateCache.Locale != CultureInfo.CurrentUICulture.Name)
             {
-                if (mountsPoints.TryGetValue(template.MountPointUri, out var existingTime))
-                    if (template.ConfigTimestampUtc < existingTime)
-                        continue;//existing time in dictionary is newer, hence don't replace it
-                mountsPoints[template.MountPointUri] = template.ConfigTimestampUtc;
-            }
-
-            var completedTask = await _templatesSourcesManager.GetTemplatesSources(forceRebuild).ConfigureAwait(false);
-            var mountsPointsInUsedNow = new HashSet<string>();
-            foreach (var source in completedTask)
-            {
-                mountsPointsInUsedNow.Add(source.MountPointUri);
-                if (mountsPoints.TryGetValue(source.MountPointUri, out var lastChangeTime))
-                {
-                    if (source.LastChangeTime > lastChangeTime)
-                        placesThatNeedScanning.Add(source.MountPointUri);
-                }
-                else
+                _userTemplateCache = new TemplateCache(_environmentSettings);
+                var completedTask = await _templatesSourcesManager.GetTemplatesSources(forceRebuild).ConfigureAwait(false);
+                foreach (var source in completedTask)
                 {
                     placesThatNeedScanning.Add(source.MountPointUri);
+                    mountsPointsInUseNow.Add(source.MountPointUri);
+                }
+            }
+            else
+            {
+                var existingMountPoints = new Dictionary<string, DateTime?>();
+
+                foreach (var template in _userTemplateCache.TemplateInfo)
+                {
+                    if (existingMountPoints.TryGetValue(template.MountPointUri, out var existingTime))
+                        if (template.ConfigTimestampUtc < existingTime)
+                            continue;//existing time in dictionary is newer, hence don't replace it
+                    existingMountPoints[template.MountPointUri] = template.ConfigTimestampUtc;
+                }
+
+                var completedTask = await _templatesSourcesManager.GetTemplatesSources(forceRebuild).ConfigureAwait(false);
+                foreach (var source in completedTask)
+                {
+                    mountsPointsInUseNow.Add(source.MountPointUri);
+                    if (existingMountPoints.TryGetValue(source.MountPointUri, out var lastScanTime))
+                    {
+                        if (source.LastChangeTime > lastScanTime)
+                            placesThatNeedScanning.Add(source.MountPointUri);
+                    }
+                    else
+                    {
+                        placesThatNeedScanning.Add(source.MountPointUri);
+                    }
                 }
             }
 
-            var removedMountPoints = mountsPoints.Keys.Except(mountsPointsInUsedNow);
-
-            TemplateCache workingCache = new TemplateCache(_environmentSettings);
             foreach (var place in placesThatNeedScanning)
             {
-                workingCache.Scan(place);
-            }
-
-            Save(workingCache);
-        }
-
-        private void ReloadTemplates()
-        {
-            _templatesLoaded = false;
-            EnsureTemplatesLoaded();
-        }
-
-        public bool IsVersionCurrent
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(_userSettings.Version) || !string.Equals(_userSettings.Version, SettingsStore.CurrentVersion, StringComparison.OrdinalIgnoreCase))
+                try
                 {
-                    return false;
+                    _userTemplateCache.Scan(place);
                 }
-
-                return true;
+                catch (Exception ex)
+                {
+                    _environmentSettings.Host.OnNonCriticalError(null, $"Failed to scan \"{place}\":{Environment.NewLine}{ex}", null, 0);
+                }
             }
+
+            // When writing the template caches, we need the existing cache version to read the existing caches for before updating.
+            // so don't update it until after the template caches are written.
+            _userTemplateCache.WriteTemplateCaches(mountsPointsInUseNow);
         }
 
         public ITemplate LoadTemplate(ITemplateInfo info, string baselineName)
@@ -370,39 +323,11 @@ namespace Microsoft.TemplateEngine.Edge.Settings
 
         public ITemplatesSourcesManager TemplatesSourcesManager => _templatesSourcesManager;
 
-        public void GetTemplates(HashSet<ITemplateInfo> templates)
+        public async Task<IReadOnlyList<ITemplateInfo>> GetTemplatesAsync(CancellationToken token)
         {
-            using (Timing.Over(_environmentSettings.Host, "Settings init"))
-                EnsureLoaded();
-            using (Timing.Over(_environmentSettings.Host, "Template load"))
-                UpdateTemplateListFromCache(_userTemplateCache, templates);
-        }
-
-        public void WriteTemplateCache(IList<ITemplateInfo> templates, string locale)
-        {
-            WriteTemplateCache(templates, locale, true);
-        }
-
-        public void WriteTemplateCache(IList<ITemplateInfo> templates, string locale, bool hasContentChanges)
-        {
-            List<TemplateInfo> toCache = templates.Cast<TemplateInfo>().ToList();
-
-            if (hasContentChanges)
-            {
-                TemplateCache cache = new TemplateCache(_environmentSettings, toCache);
-                JObject serialized = JObject.FromObject(cache);
-                _paths.WriteAllText(_paths.User.ExplicitLocaleTemplateCacheFile(locale), serialized.ToString());
-            }
-
-            CultureInfo currentUICulture = CultureInfo.CurrentUICulture;
-            bool isCurrentLocale = string.IsNullOrEmpty(locale) && currentUICulture == CultureInfo.InvariantCulture
-                || locale == currentUICulture.Name;
-
-            // TODO: determine if this reload is necessary if there wasn't a save (probably not needed)
-            if (isCurrentLocale)
-            {
-                ReloadTemplates();
-            }
+            EnsureLoaded();
+            await RebuildCacheFromSettingsIfNotCurrent(false).ConfigureAwait(false);
+            return _userTemplateCache.TemplateInfo;
         }
 
         public void AddProbingPath(string probeIn)
@@ -427,7 +352,6 @@ namespace Microsoft.TemplateEngine.Edge.Settings
                 catch
                 {
                     Task.Delay(10).Wait();
-                    Reload();
                 }
             }
         }
