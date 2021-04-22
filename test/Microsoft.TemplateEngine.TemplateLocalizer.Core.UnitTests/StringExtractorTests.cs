80d4da5de02ff27c4565271aaafc0b3a79b8fbe5
Bekir Ozturk <bozturk@microsoft.com>
2021-04-22 16:41:39 +0200

    TemplateLocalizer Core Tests

---
+++ test/Microsoft.TemplateEngine.TemplateLocalizer.Core.UnitTests/StringExtractorTests.cs
@@ -0,0 +1,172 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.TemplateEngine.TemplateLocalizer.Core.UnitTests
+{
+    [TestClass]
+    public class StringExtractorTests
+    {
+        private const string SimpleTemplateJson = @"{
+    ""$schema"": ""http://json.schemastore.org/template"",
+    ""author"": ""Microsoft"",
+    ""classifications"": [""Common"", ""Library""],
+    ""name"": ""Class library"",
+    ""generatorVersions"": ""[1.0.0.0-*)"",
+    ""description"": ""dEscRiPtiON: ,./|\\<>{}!@#$%^&*()_+-=? 12 äÄßöÖüÜçÇğĞıIİşŞ"",
+    ""groupIdentity"": ""Microsoft.Common.Library"",
+    ""precedence"": ""7000"",
+    ""identity"": ""Microsoft.Common.Library.CSharp.5.0"",
+    ""shortName"": ""classlib""
+}
+";
+        private const string ComplexTemplateJson = @"{
+    ""$schema"": ""http://json.schemastore.org/template"",
+    ""author"": ""Microsoft"",
+    ""classifications"": [""Common"", ""Library""],
+    ""name"": ""Class library"",
+    ""generatorVersions"": ""[1.0.0.0-*)"",
+    ""description"": ""dEscRiPtiON: ,./|\\<>{}!@#$%^&*()_+-=? 12 äÄßöÖüÜçÇğĞıIİşŞ"",
+    ""symbols"": {
+        ""TargetFrameworkOverride"": {
+          ""type"": ""parameter"",
+          ""displayName"": ""tfm display name"",
+          ""description"": ""tfm description"",
+          ""replaces"": ""TargetFrameworkOverride"",
+          ""datatype"": ""string"",
+          ""defaultValue"": """"
+        },
+        ""Framework"": {
+          ""type"": ""parameter"",
+          ""displayName"": ""framework display name"",
+          ""description"": ""framework description"",
+          ""datatype"": ""choice"",
+          ""choices"": [
+            {
+              ""choice"": ""net5.0"",
+              ""displayName"": ""net5.0 display name"",
+              ""description"": ""Target net5.0""
+            },
+            {
+              ""choice"": ""netstandard2.1"",
+              ""description"": ""Target netstandard2.1""
+            },
+            {
+              ""choice"": ""netstandard2.0"",
+              ""displayName"": ""netstandard2.0 display name"",
+              ""description"": ""Target netstandard2.0""
+            }
+          ],
+          ""replaces"": ""net5.0"",
+          ""defaultValue"": ""net5.0""
+        },
+    },
+    ""postActions"": [
+        {
+          ""condition"": ""(!skipRestore)"",
+          ""description"": ""Restore NuGet packages required by this project."",
+          ""manualInstructions"": [
+            {
+                ""text"": ""Run 'dotnet restore'""
+            }
+          ],
+          ""actionId"": ""210D431B-A78B-4D2F-B762-4ED3E3EA9025"",
+          ""continueOnError"": true
+        },
+        {
+        ""condition"": ""(HostIdentifier != \""dotnetcli\"" && HostIdentifier != \""dotnetcli-preview\"")"",
+          ""description"": ""Opens Class1.cs in the editor"",
+          ""manualInstructions"": [ ],
+          ""actionId"": ""84C0DA21-51C8-4541-9940-6CA19AF04EE6"",
+          ""args"": {
+            ""files"": ""1""
+          },
+          ""continueOnError"": true
+        }
+      ]
+}
+";
+
+        [TestMethod]
+        public void FirstLevelStringsAreExtracted()
+        {
+            var strings = ExtractStrings(SimpleTemplateJson, out _);
+
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..name" && s.LocalizationKey == "name" && s.Value == "Class library"));
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..description" && s.LocalizationKey == "description" && s.Value == "dEscRiPtiON: ,./|\\<>{}!@#$%^&*()_+-=? 12 äÄßöÖüÜçÇğĞıIİşŞ"));
+        }
+
+        [TestMethod]
+        public void CertainStringsAreOmitted()
+        {
+            var strings = ExtractStrings(SimpleTemplateJson, out _);
+
+            Assert.IsTrue(!strings.Any(s => s.Identifier == "..$schema" && s.LocalizationKey == "author" && s.Value == "Microsoft"));
+            Assert.IsTrue(!strings.Any(s => s.Identifier == "..classification" || s.LocalizationKey == "classification"));
+            Assert.IsTrue(!strings.Any(s => s.Identifier == "..groupIdentity" || s.LocalizationKey == "groupIdentity"));
+        }
+
+        [TestMethod]
+        public void DefaultAuthoringLanguageIsEnglish()
+        {
+            _ = ExtractStrings(SimpleTemplateJson, out string language);
+
+            Assert.IsTrue(language == "en");
+        }
+
+        [TestMethod]
+        public void SymbolsAreExtracted()
+        {
+            var strings = ExtractStrings(ComplexTemplateJson, out _);
+
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..symbols.targetframeworkoverride.description" && s.LocalizationKey == "symbols.TargetFrameworkOverride.description" && s.Value == "tfm description"));
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..symbols.targetframeworkoverride.displayname" && s.LocalizationKey == "symbols.TargetFrameworkOverride.displayName" && s.Value == "tfm display name"));
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..symbols.framework.description" && s.LocalizationKey == "symbols.Framework.description" && s.Value == "framework description"));
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..symbols.framework.displayname" && s.LocalizationKey == "symbols.Framework.displayName" && s.Value == "framework display name"));
+        }
+
+        [TestMethod]
+        public void SymbolChoicesAreExtracted()
+        {
+            var strings = ExtractStrings(ComplexTemplateJson, out _);
+
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..symbols.framework.choices.0.description" && s.LocalizationKey == "symbols.Framework.choices.net5_0.description" && s.Value == "Target net5.0"));
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..symbols.framework.choices.0.displayname" && s.LocalizationKey == "symbols.Framework.choices.net5_0.displayName" && s.Value == "net5.0 display name"));
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..symbols.framework.choices.2.description" && s.LocalizationKey == "symbols.Framework.choices.netstandard2_0.description" && s.Value == "Target netstandard2.0"));
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..symbols.framework.choices.2.displayname" && s.LocalizationKey == "symbols.Framework.choices.netstandard2_0.displayName" && s.Value == "netstandard2.0 display name"));
+        }
+
+        [TestMethod]
+        public void PostActionsAreExtracted()
+        {
+            var strings = ExtractStrings(ComplexTemplateJson, out _);
+
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..postactions.0.description" && s.LocalizationKey == "postActions[0].description" && s.Value == "Restore NuGet packages required by this project."));
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..postactions.1.description" && s.LocalizationKey == "postActions[1].description" && s.Value == "Opens Class1.cs in the editor"));
+        }
+
+        [TestMethod]
+        public void ManualInstructionsAreExtracted()
+        {
+            var strings = ExtractStrings(ComplexTemplateJson, out _);
+
+            Assert.IsTrue(strings.Any(s => s.Identifier == "..postactions.0.manualinstructions.0.text" && s.LocalizationKey == "postActions[0].manualInstructions[0].text" && s.Value == "Run 'dotnet restore'"));
+        }
+
+        private static IReadOnlyList<TemplateString> ExtractStrings(string json, out string language)
+        {
+            JsonDocument jsonDocument = JsonDocument.Parse(json, new JsonDocumentOptions()
+            {
+                CommentHandling = JsonCommentHandling.Skip,
+                AllowTrailingCommas = true,
+            });
+
+            TemplateStringExtractor templateStringExtractor = new TemplateStringExtractor(jsonDocument);
+            return templateStringExtractor.ExtractStrings(out language);
+        }
+    }
+}
---
+++ test/Microsoft.TemplateEngine.TemplateLocalizer.Core.UnitTests/StringUpdaterTests.cs
@@ -0,0 +1,154 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.TemplateEngine.TemplateLocalizer.Core.UnitTests
+{
+    [TestClass]
+    public class StringUpdaterTests
+    {
+        private static readonly IReadOnlyList<TemplateString> InputStrings = new List<TemplateString>()
+        {
+            new ("..name", "name", "Class library"),
+            new ("..description", "description", "dEscRiPtiON: ,./|\\<>{}!@#$%^&*()_+-=? 12 äÄßöÖüÜçÇğĞıIİşŞ"),
+            new ("..symbols.targetframeworkoverride.displayname", "symbols.TargetFrameworkOverride.displayName", "tfm display name"),
+            new ("..symbols.targetframeworkoverride.description", "symbols.TargetFrameworkOverride.description", "tfm description"),
+            new ("..symbols.framework.displayname", "symbols.Framework.displayName", "framework display name"),
+            new ("..symbols.framework.description", "symbols.Framework.description", "framework description"),
+            new ("..symbols.framework.choices.0.displayname", "symbols.Framework.choices.net5_0.displayName", "net5.0 display name"),
+            new ("..symbols.framework.choices.0.description", "symbols.Framework.choices.net5_0.description", "Target net5.0"),
+            new ("..symbols.framework.choices.1.description", "symbols.Framework.choices.netstandard2_1.description", "Target netstandard2.1"),
+            new ("..symbols.framework.choices.2.displayname", "symbols.Framework.choices.netstandard2_0.displayName", "netstandard2.0 display name"),
+            new ("..symbols.framework.choices.2.description", "symbols.Framework.choices.netstandard2_0.description", "Target netstandard2.0"),
+            new ("..postactions.0.description", "postActions[0].description", "Restore NuGet packages required by this project."),
+            new ("..postactions.0.manualinstructions.0.text", "postActions[0].manualInstructions[0].text", "Run 'dotnet restore'"),
+            new ("..postactions.1.description", "postActions[1].description", "Opens Class1.cs in the editor")
+        };
+
+        private string? _workingDirectory;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _workingDirectory = Path.Combine(Path.GetTempPath(), "Microsoft.TemplateEngine.TemplateLocalizer.Core.UnitTests", Path.GetRandomFileName());
+            Directory.CreateDirectory(_workingDirectory);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Directory.Delete(_workingDirectory, true);
+        }
+
+        [TestMethod]
+        public async Task AllStringsAreWrittenToFile()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource(10000);
+            await TemplateStringUpdater.UpdateStringsAsync(InputStrings, "en", new string[] { "tr" }, _workingDirectory, NullLogger.Instance, cts.Token)
+                .ConfigureAwait(false);
+
+            string expectedFilename = Path.Combine(_workingDirectory, "tr.templatestrings.json");
+            Assert.IsTrue(File.Exists(expectedFilename));
+
+            // Only the specified language file should be generated (no more than 1 file in the directory).
+            Assert.IsTrue(Directory.EnumerateFileSystemEntries(_workingDirectory).Count() == 1);
+
+            Dictionary<string, string> resultStrings = await ReadTemplateStringsFromJsonFile(expectedFilename, cts.Token).ConfigureAwait(false);
+
+            // All the InputStrings should be in the resultStrings
+            Assert.IsTrue(InputStrings.All(i => resultStrings.TryGetValue(i.LocalizationKey, out var value) && value == i.Value));
+        }
+
+        [TestMethod]
+        public async Task StringOrderIsPreserved()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource(10000);
+            await TemplateStringUpdater.UpdateStringsAsync(InputStrings, "en", new string[] { "tr" }, _workingDirectory, NullLogger.Instance, cts.Token)
+                .ConfigureAwait(false);
+
+            string expectedFilename = Path.Combine(_workingDirectory, "tr.templatestrings.json");
+            Assert.IsTrue(File.Exists(expectedFilename));
+
+            using FileStream locFileStream = File.OpenRead(expectedFilename);
+            JsonDocument locFile = await JsonDocument.ParseAsync(locFileStream).ConfigureAwait(false);
+
+            int inputIndex = 0;
+            foreach (JsonProperty property in locFile.RootElement.EnumerateObject())
+            {
+                if (inputIndex >= InputStrings.Count)
+                {
+                    return;
+                }
+
+                Assert.IsTrue(property.Name == InputStrings[inputIndex].LocalizationKey);
+                Assert.IsTrue(property.Value.GetString() == InputStrings[inputIndex].Value);
+                inputIndex++;
+            }
+        }
+
+        [TestMethod]
+        public async Task ExistingTranslationsAndCommentsArePreserved()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource(10000);
+            string expectedFilename = Path.Combine(_workingDirectory, "tr.templatestrings.json");
+
+            await File.WriteAllTextAsync(expectedFilename, @"
+{
+    ""name"": ""existing translations should be preserved."",
+    ""_name.comment"": ""comments should be preserved.""
+}",
+                cts.Token).ConfigureAwait(false);
+
+            await TemplateStringUpdater.UpdateStringsAsync(InputStrings, "en", new string[] { "tr" }, _workingDirectory, NullLogger.Instance, cts.Token)
+                .ConfigureAwait(false);
+
+            string fileContent = await File.ReadAllTextAsync(expectedFilename, cts.Token).ConfigureAwait(false);
+
+            Assert.IsTrue(fileContent.Contains("existing translations should be preserved."));
+            Assert.IsTrue(fileContent.Contains("comments should be preserved."));
+        }
+
+        [TestMethod]
+        public async Task ExistingValuesOfAuthoringLanguageShouldBeRemoved()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource(10000);
+            string expectedFilename = Path.Combine(_workingDirectory, "en.templatestrings.json");
+
+            await File.WriteAllTextAsync(expectedFilename, @"
+{
+    ""name"": ""existing translations in authoring language should be removed.""
+}",
+                cts.Token).ConfigureAwait(false);
+
+            await TemplateStringUpdater.UpdateStringsAsync(InputStrings, "en", new string[] { "en" }, _workingDirectory, NullLogger.Instance, cts.Token)
+                .ConfigureAwait(false);
+
+            string fileContent = await File.ReadAllTextAsync(expectedFilename, cts.Token).ConfigureAwait(false);
+
+            Assert.IsFalse(fileContent.Contains("existing translations in authoring language should be removed."));
+        }
+
+        private static async Task<Dictionary<string, string>> ReadTemplateStringsFromJsonFile(string path, CancellationToken cancellationToken)
+        {
+            using FileStream openStream = File.OpenRead(path);
+            JsonSerializerOptions serializerOptions = new()
+            {
+                AllowTrailingCommas = true,
+                MaxDepth = 1,
+            };
+
+            return await JsonSerializer.DeserializeAsync<Dictionary<string, string>>(openStream, serializerOptions, cancellationToken)
+                .ConfigureAwait(false)
+                ?? new Dictionary<string, string>();
+        }
+    }
+}
