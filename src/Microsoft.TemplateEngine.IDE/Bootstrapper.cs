227da457c46420b161673a5734a75e5b98353ab2
Vlada Shubina <vshubina@microsoft.com>
2021-05-18 16:58:14 +0300

    fixes dotnet/templating#2613 Provide the implementation for registering default assemblies in Bootstrapper
    # Conflicts:
    #	src/Microsoft.TemplateEngine.IDE/PublicAPI.Unshipped.txt

--- src/Microsoft.TemplateEngine.IDE/Bootstrapper.cs
+++ src/Microsoft.TemplateEngine.IDE/Bootstrapper.cs
@@ -26,11 +26,22 @@ namespace Microsoft.TemplateEngine.IDE
         private readonly TemplateCreator _templateCreator;
         private readonly EngineEnvironmentSettings _engineEnvironmentSettings;
 
-        public Bootstrapper(ITemplateEngineHost host, Action<IEngineEnvironmentSettings> onFirstRun, bool virtualizeConfiguration)
+        /// <summary>
+        /// Creates the instance.
+        /// </summary>
+        /// <param name="host">caller <see cref="ITemplateEngineHost"/>.</param>
+        /// <param name="onFirstRun">action to perform on the first run of template engine host.</param>
+        /// <param name="virtualizeConfiguration">if true, settings will be stored in memory and will be disposed with instance.</param>
+        /// <param name="loadDefaultComponents">if true, the default components (providers, installers, generator) will be loaded. Same as calling <see cref="LoadDefaultComponents()"/> after instance is created.</param>
+        public Bootstrapper(ITemplateEngineHost host, Action<IEngineEnvironmentSettings> onFirstRun, bool virtualizeConfiguration, bool loadDefaultComponents = true)
         {
             _host = host ?? throw new ArgumentNullException(nameof(host));
             _engineEnvironmentSettings = new EngineEnvironmentSettings(host, virtualizeSettings: virtualizeConfiguration, onFirstRun: onFirstRun);
             _templateCreator = new TemplateCreator(_engineEnvironmentSettings);
+            if (loadDefaultComponents)
+            {
+                LoadDefaultComponents();
+            }
         }
 
         [Obsolete("Use ITemplateEngineHost.BuiltInComponents or AddComponent to add components.")]
@@ -43,6 +54,21 @@ namespace Microsoft.TemplateEngine.IDE
         public void Register(Assembly assembly)
         {
             _engineEnvironmentSettings.SettingsLoader.Components.RegisterMany(assembly.GetTypes());
+        }
+
+        /// <summary>
+        /// Loads default components: template package providers and installers defined in Microsoft.TemplateEngine.Edge and default template generator defined in Microsoft.TemplateEngine.Orchestrator.RunnableProjects.
+        /// </summary>
+        public void LoadDefaultComponents()
+        {
+            foreach ((Type Type, IIdentifiedComponent Instance) component in Orchestrator.RunnableProjects.Components.AllComponents)
+            {
+                AddComponent(component.Type, component.Instance);
+            }
+            foreach ((Type Type, IIdentifiedComponent Instance) component in Edge.Components.AllComponents)
+            {
+                AddComponent(component.Type, component.Instance);
+            }
         }
 
         /// <summary>
