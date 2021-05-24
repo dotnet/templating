c93244c11dd52f37620d31d8254916d4b52acad7
Vlada Shubina <vshubina@microsoft.com>
2021-05-24 12:38:33 +0300

    fixes dotnet/templating#2994 adds localization for error messages in Edge

--- src/Microsoft.TemplateEngine.Edge/Template/TemplateCreator.cs
+++ src/Microsoft.TemplateEngine.Edge/Template/TemplateCreator.cs
@@ -60,7 +60,7 @@ namespace Microsoft.TemplateEngine.Edge.Template
             ITemplate? template = LoadTemplate(templateInfo, baselineName);
             if (template == null)
             {
-                return new TemplateCreationResult(CreationResultStatus.NotFound, templateInfo.Name, "Could not load template");
+                return new TemplateCreationResult(CreationResultStatus.NotFound, templateInfo.Name, LocalizableStrings.TemplateCreator_TemplateCreationResult_Error_CouldNotLoadTemplate);
             }
 
             string? realName = name ?? fallbackName ?? template.DefaultName;
@@ -103,7 +103,7 @@ namespace Microsoft.TemplateEngine.Edge.Template
                         return new TemplateCreationResult(
                             CreationResultStatus.DestructiveChangesDetected,
                             template.Name,
-                            "Destructive changes detected",
+                            LocalizableStrings.TemplateCreator_TemplateCreationResult_Error_DestructiveChanges,
                             null,
                             null,
                             creationEffects);
@@ -136,7 +136,7 @@ namespace Microsoft.TemplateEngine.Edge.Template
                 return new TemplateCreationResult(
                     status: CreationResultStatus.CreateFailed,
                     templateName: template.Name,
-                    localizedErrorMessage: message,
+                    localizedErrorMessage: string.Format(LocalizableStrings.TemplateCreator_TemplateCreationResult_Error_CreationFailed, message),
                     outputBaseDir: targetDir);
             }
             catch (Exception ex)
@@ -144,7 +144,7 @@ namespace Microsoft.TemplateEngine.Edge.Template
                 return new TemplateCreationResult(
                     status: CreationResultStatus.CreateFailed,
                     templateName: template.Name,
-                    localizedErrorMessage: ex.Message,
+                    localizedErrorMessage: string.Format(LocalizableStrings.TemplateCreator_TemplateCreationResult_Error_CreationFailed, ex.Message),
                     outputBaseDir: targetDir);
             }
             finally
