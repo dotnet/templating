30dec6c64acbb4f0c16555afa8ae755fa771d96a
Vlada Shubina <vshubina@microsoft.com>
2021-06-08 13:05:28 +0300

    fixes

--- src/Microsoft.TemplateEngine.TemplateLocalizer.Core/TraversalRules/RegexFilteredTraversalRule.cs
+++ src/Microsoft.TemplateEngine.TemplateLocalizer.Core/TraversalRules/RegexFilteredTraversalRule.cs
@@ -17,18 +17,18 @@ namespace Microsoft.TemplateEngine.TemplateLocalizer.Core.TraversalRules
         /// Creates an instance of <see cref="RegexFilteredTraversalRule"/>.
         /// </summary>
         /// <param name="regexPattern">Regex pattern string that will be used to match json element names.</param>
-        /// <param name="keyExtractor"><see cref="IJsonKeyCreator"/> to be used when creating a key for the match elements.</param>
-        public RegexFilteredTraversalRule(string regexPattern, IJsonKeyCreator? keyExtractor = default)
-            : this(new Regex(regexPattern), keyExtractor)
+        /// <param name="keyCreator"><see cref="IJsonKeyCreator"/> to be used when creating a key for the match elements.</param>
+        public RegexFilteredTraversalRule(string regexPattern, IJsonKeyCreator? keyCreator = default)
+            : this(new Regex(regexPattern), keyCreator)
         { }
 
         /// <summary>
         /// Creates an instance of <see cref="RegexFilteredTraversalRule"/>.
         /// </summary>
         /// <param name="regex">Regex pattern that will be used to match json element names.</param>
-        /// <param name="keyExtractor"><see cref="IJsonKeyCreator"/> to be used when creating a key for the match elements.</param>
-        public RegexFilteredTraversalRule(Regex regex, IJsonKeyCreator? keyExtractor = default)
-            : base(keyExtractor)
+        /// <param name="keyCreator"><see cref="IJsonKeyCreator"/> to be used when creating a key for the match elements.</param>
+        public RegexFilteredTraversalRule(Regex regex, IJsonKeyCreator? keyCreator = default)
+            : base(keyCreator)
         {
             _regex = regex;
         }
