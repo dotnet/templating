30ee72a701e3c6777ea232387c638e25a50fa980
Vlada Shubina <vshubina@microsoft.com>
2021-07-29 12:48:16 +0300

    draft version
    GetWidth should be replaced to wcwidth GetWidth

---
+++ src/Microsoft.TemplateEngine.Cli/TableOutput/UnicodeLength.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+namespace Microsoft.TemplateEngine.Cli.TableOutput
+{
+    internal static class UnicodeLength
+    {
+        internal static int GetUnicodeLength (this string s)
+        {
+            return s.Sum(ch => GetWidth((int)ch));
+        }
+    }
+}
