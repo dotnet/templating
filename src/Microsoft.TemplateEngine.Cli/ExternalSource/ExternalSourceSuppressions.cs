3fac98499b02206b19925c5e249fdc97ecf830a2
Bekir Ozturk <bozturk@microsoft.com>
2021-08-10 16:30:56 +0200

    Reference Wcwidth.Sources package

---
+++ src/Microsoft.TemplateEngine.Cli/ExternalSource/ExternalSourceSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0005:Using directive is unnecessary.", Justification = "Code-Only packages cannot be edited to conform to project settings.", Scope = "module")]
+[assembly: SuppressMessage("ApiDesign", "RS0016:Add public types and members to the declared API", Justification = "Code-Only packages cannot be edited to conform to project settings.", Scope = "namespaceanddescendants", Target = "Wcwidth")]
+[assembly: SuppressMessage("Performance", "CA1810:Initialize reference type static fields inline", Justification = "Code-Only packages cannot be edited to conform to project settings.", Scope = "namespaceanddescendants", Target = "Wcwidth")]
+[assembly: SuppressMessage("ApiDesign", "RS0026:Do not add multiple public overloads with optional parameters", Justification = "Code-Only packages cannot be edited to conform to project settings.", Scope = "namespaceanddescendants", Target = "Wcwidth")]
+[assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1404:Code analysis suppression should have justification", Justification = "Code-Only packages cannot be edited to conform to project settings.", Scope = "namespaceanddescendants", Target = "Wcwidth")]
