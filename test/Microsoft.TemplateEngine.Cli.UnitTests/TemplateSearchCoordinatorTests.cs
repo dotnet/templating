c9eb3b2f248fd5d2ea3cf0d2722b5ad02584a816
Vlada Shubina <vshubina@microsoft.com>
2021-10-14 10:28:28 +0200

    Template Discovery: fixed the issue when cli host data was not read from existing cache in diff mode

--- test/Microsoft.TemplateEngine.Cli.UnitTests/TemplateSearchCoordinatorTests.cs
+++ test/Microsoft.TemplateEngine.Cli.UnitTests/TemplateSearchCoordinatorTests.cs
@@ -514,7 +514,7 @@ namespace Microsoft.TemplateEngine.Cli.UnitTests
                     { _fooTwoTemplate.Identity, fooTemplateHostData }
                 };
 
-                additionalData[CliTemplateSearchCoordinatorFactory.CliHostDataName] = cliHostData;
+                additionalData[CliHostSearchCacheData.DataName] = cliHostData;
             }
 
             TemplateDiscoveryMetadata discoveryMetadata = new TemplateDiscoveryMetadata(version, templateCache, packToTemplateMap, additionalData);
@@ -541,7 +541,7 @@ namespace Microsoft.TemplateEngine.Cli.UnitTests
 
             Dictionary<string, object> additionalData = new Dictionary<string, object>()
             {
-                { CliTemplateSearchCoordinatorFactory.CliHostDataName, fooTemplateHostData }
+                { CliHostSearchCacheData.DataName, fooTemplateHostData }
             };
             List<ITemplateInfo> templateCache = new List<ITemplateInfo>();
 
