9baecd9e6696e9fff67343a259884193c518c035
Vlada Shubina <vshubina@microsoft.com>
2021-11-12 13:17:49 +0200

    default if no option value and hex type implementation

--- test/Microsoft.TemplateEngine.Mocks/MockTemplateInfo.cs
+++ test/Microsoft.TemplateEngine.Mocks/MockTemplateInfo.cs
@@ -171,7 +171,7 @@ namespace Microsoft.TemplateEngine.Mocks
             return this;
         }
 
-        public MockTemplateInfo WithChoiceParameter(string name, string[] values, bool isRequired = false, string? defaultValue = null)
+        public MockTemplateInfo WithChoiceParameter(string name, string[] values, bool isRequired = false, string? defaultValue = null, string? defaultIfNoOptionValue = null)
         {
             _parameters.Add(name, new TemplateParameter(
                 name,
@@ -179,6 +179,7 @@ namespace Microsoft.TemplateEngine.Mocks
                 datatype: "choice",
                 priority: isRequired ? TemplateParameterPriority.Required : TemplateParameterPriority.Optional,
                 defaultValue: defaultValue,
+                defaultIfOptionWithoutValue: defaultIfNoOptionValue,
                 choices: values.ToDictionary(v => v, v => new ParameterChoice(null, null))));
             return this;
         }
@@ -215,14 +216,15 @@ namespace Microsoft.TemplateEngine.Mocks
             return this;
         }
 
-        public MockTemplateInfo WithParameter(string paramName, string paramType = "string", bool isRequired = false, string? defaultValue = null)
+        public MockTemplateInfo WithParameter(string paramName, string paramType = "string", bool isRequired = false, string? defaultValue = null, string? defaultIfNoOptionValue = null)
         {
             _parameters[paramName] = new TemplateParameter(
                 paramName,
                 "parameter",
                 paramType,
                 isRequired ? TemplateParameterPriority.Required : TemplateParameterPriority.Optional,
-                defaultValue: defaultValue);
+                defaultValue: defaultValue,
+                defaultIfOptionWithoutValue: defaultIfNoOptionValue);
             return this;
         }
 
