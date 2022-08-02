1c75fd3d4fbe6646266828feda3f4ddc57ed03e7
Jan Krivanek <jankrivanek@microsoft.com>
2022-08-02 11:29:02 +0200

    Add diffplex

--- test/dotnet-new3.UnitTests/ModuleInitializer.cs
+++ test/dotnet-new3.UnitTests/ModuleInitializer.cs
@@ -10,11 +10,15 @@ namespace Dotnet_new3.IntegrationTests
         [ModuleInitializer]
         public static void Init()
         {
+            // Customization of storage of .verified comparison files (see https://github.com/VerifyTests/Verify/blob/main/docs/naming.md#derivepathinfo).
             VerifierSettings.DerivePathInfo(
                 (_, _, type, method) => new(
                     directory: "Approvals",
                     typeName: type.Name,
                     methodName: method.Name));
+
+            // Customize diff output of verifier
+            VerifyDiffPlex.Initialize(OutputType.Compact);
         }
     }
 }
