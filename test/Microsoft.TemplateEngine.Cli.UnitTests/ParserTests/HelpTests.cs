94e36d6339a40a2b2d6a50a8751ef30bb5ac1eb8
Jan Krivanek <jankrivanek@microsoft.com>
2022-08-02 17:36:36 +0200

    Fix build

--- test/Microsoft.TemplateEngine.Cli.UnitTests/ParserTests/HelpTests.cs
+++ test/Microsoft.TemplateEngine.Cli.UnitTests/ParserTests/HelpTests.cs
@@ -6,6 +6,7 @@ using System.CommandLine.Help;
 using System.CommandLine.Parsing;
 using FakeItEasy;
 using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Cli.Commands;
 using Microsoft.TemplateEngine.Edge;
 using Microsoft.TemplateEngine.Edge.Settings;
 using Microsoft.TemplateEngine.Mocks;
