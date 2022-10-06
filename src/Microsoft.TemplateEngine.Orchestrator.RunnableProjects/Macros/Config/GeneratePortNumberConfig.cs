756344d782d0d811712710e12c5e0473dbfc9915
kasperk81 <83082615+kasperk81@users.noreply.github.com>
2022-10-06 23:38:33 +0300

    block all unsafe ports
    extracted from three browsers' sources: https://github.com/dotnet/runtime/pull/76236#issuecomment-1262346000.

--- src/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/Macros/Config/GeneratePortNumberConfig.cs
+++ src/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/Macros/Config/GeneratePortNumberConfig.cs
@@ -12,15 +12,88 @@ namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.Macros.Config
     {
         private static readonly HashSet<int> UnsafePorts = new HashSet<int>()
         {
-                    2049, // nfs
-                    3659, // apple-sasl / PasswordServer
-                    4045, // lockd
-                    6000, // X11
-                    6665, // Alternate IRC [Apple addition]
-                    6666, // Alternate IRC [Apple addition]
-                    6667, // Standard IRC [Apple addition]
-                    6668, // Alternate IRC [Apple addition]
-                    6669, // Alternate IRC [Apple addition]
+            1, // tcpmux
+            7, // echo
+            9, // discard
+            11, // systat
+            13, // daytime
+            15, // netstat
+            17, // qotd
+            19, // chargen
+            20, // FTP-data
+            21, // FTP-control
+            22, // SSH
+            23, // telnet
+            25, // SMTP
+            37, // time
+            42, // name
+            43, // nicname
+            53, // domain
+            69, // TFTP
+            77, // priv-rjs
+            79, // finger
+            87, // ttylink
+            95, // supdup
+            101, // hostriame
+            102, // iso-tsap
+            103, // gppitnp
+            104, // acr-nema
+            109, // POP2
+            110, // POP3
+            111, // sunrpc
+            113, // auth
+            115, // SFTP
+            117, // uucp-path
+            119, // nntp
+            123, // NTP
+            135, // loc-srv / epmap
+            137, // NetBIOS
+            139, // netbios
+            143, // IMAP2
+            161, // SNMP
+            179, // BGP
+            389, // LDAP
+            427, // SLP (Also used by Apple Filing Protocol)
+            465, // SMTP+SSL
+            512, // print / exec
+            513, // login
+            514, // shell
+            515, // printer
+            526, // tempo
+            530, // courier
+            531, // Chat
+            532, // netnews
+            540, // UUCP
+            548, // afpovertcp [Apple addition]
+            554, // rtsp
+            556, // remotefs
+            563, // NNTP+SSL
+            587, // ESMTP
+            601, // syslog-conn
+            636, // LDAP+SSL
+            989, // ftps-data
+            990, // ftps
+            993, // IMAP+SSL
+            995, // POP3+SSL
+            1719, // H323 (RAS)
+            1720, // H323 (Q931)
+            1723, // H323 (H245)
+            2049, // NFS
+            3659, // apple-sasl / PasswordServer [Apple addition]
+            4045, // lockd
+            5060, // SIP
+            5061, // SIPS
+            6000, // X11
+            6566, // SANE
+            6665, // Alternate IRC [Apple addition]
+            6666, // Alternate IRC [Apple addition]
+            6667, // Standard IRC [Apple addition]
+            6668, // Alternate IRC [Apple addition]
+            6669, // Alternate IRC [Apple addition]
+            6697, // IRC+SSL [Apple addition]
+            10080, // amanda
+            4190, // ManageSieve [Apple addition]
+            6679, // Alternate IRC SSL [Apple addition]
         };
 
         internal GeneratePortNumberConfig(string variableName, string? dataType, int fallback, int low, int high)
