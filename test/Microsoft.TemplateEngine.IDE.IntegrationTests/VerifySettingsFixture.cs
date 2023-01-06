39fd4670de1228d39d998f72b0fe998c07095b47
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2023-01-06 14:36:29 +0100

    update VerifySettingsFixture according to version upgrade

--- test/Microsoft.TemplateEngine.IDE.IntegrationTests/VerifySettingsFixture.cs
+++ test/Microsoft.TemplateEngine.IDE.IntegrationTests/VerifySettingsFixture.cs
@@ -1,24 +1,27 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using VerifyTests;
+using VerifyTests.DiffPlex;
 
 namespace Microsoft.TemplateEngine.IDE.IntegrationTests
 {
     public class VerifySettingsFixture : IDisposable
     {
-        public VerifySettingsFixture()
+        private static readonly Lazy<bool> Called = new Lazy<bool>(() =>
         {
-            VerifierSettings.DerivePathInfo(
-                (_, _, type, method) => new(
-                    directory: "Approvals",
-                    typeName: type.Name,
-                    methodName: method.Name));
+            DerivePathInfo(
+               (_, _, type, method) => new(
+                   directory: "Approvals",
+                   typeName: type.Name,
+                   methodName: method.Name));
 
             // Customize diff output of verifier
             VerifyDiffPlex.Initialize(OutputType.Compact);
-        }
+
+            return true;
+        });
+
+        public VerifySettingsFixture() => _ = Called.Value;
 
         public void Dispose() { }
     }
