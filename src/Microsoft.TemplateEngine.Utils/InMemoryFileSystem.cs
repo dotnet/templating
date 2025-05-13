b326caa2579d8604cac4ea8f5eb805658d0f2f14
Lance Barnes <lancerbarnes@gmail.com>
2025-05-12 18:17:52 -0600

    Remove Console WriteLine

--- src/Microsoft.TemplateEngine.Utils/InMemoryFileSystem.cs
+++ src/Microsoft.TemplateEngine.Utils/InMemoryFileSystem.cs
@@ -81,8 +81,6 @@ namespace Microsoft.TemplateEngine.Utils
                 currentDir = dir;
             }
 
-            Console.WriteLine($"Creating file {parts[parts.Length - 1]} in {currentDir.FullPath}");
-
             if (!currentDir.Files.TryGetValue(parts[parts.Length - 1], out FileSystemFile targetFile))
             {
                 targetFile = new FileSystemFile(parts[parts.Length - 1], Path.Combine(currentDir.FullPath, parts[parts.Length - 1]));
